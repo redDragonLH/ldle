d3cf4b13101fae4de251c03815e156e071d88102
liuhe <ok1332983774@163.com>
2021-07-28 13:42:34 +0800

    863. 二叉树中所有距离为 K 的结点 官方题解

--- leetcode/algorithm/863-all-nodes-distance-k-in-binary-tree.js
+++ leetcode/algorithm/863-all-nodes-distance-k-in-binary-tree.js
@@ -42,4 +42,51 @@ var distanceK = function (root, target, k) {
   if (targetIndex === -1) return [];
   // 下/上
   // 下好说,当前 targetIndex + K得到的
+  // 往上获取要处理分叉,很麻烦
+};
+
+/**
+ * 官方题解 
+ * 从 target 作为根节点遍历元素,在此之前处理数据保证可以遍历到父元素
+ */
+ var distanceK = function(root, target, k) {
+    const parents = new Map();
+    const ans = [];
+
+    const findParents = (node) => {
+        if (node.left != null) {
+            parents.set(node.left.val, node);
+            findParents(node.left);
+        }
+        if (node.right != null) {
+            parents.set(node.right.val, node);
+            findParents(node.right);
+        }
+    }
+
+    // 从 root 出发 DFS，记录每个结点的父结点
+    findParents(root);
+
+    const findAns = (node, from, depth, k) => {
+        if (node == null) {
+            return;
+        }
+        if (depth === k) {
+            ans.push(node.val);
+            return;
+        }
+        if (node.left !== from) {
+            findAns(node.left, node, depth + 1, k);
+        }
+        if (node.right !== from) {
+            findAns(node.right, node, depth + 1, k);
+        }
+        if (parents.get(node.val) !== from) {
+            findAns(parents.get(node.val), node, depth + 1, k);
+        }
+    }
+    // 从 target 出发 DFS，寻找所有深度为 k 的结点
+    findAns(target, null, 0, k);
+
+    return ans;
 };
