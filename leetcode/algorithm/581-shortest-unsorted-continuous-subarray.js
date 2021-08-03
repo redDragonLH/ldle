da12eae2fd13c5286c8139d011626a99f1ac7d87
liuhe <ok1332983774@163.com>
2021-08-03 10:33:59 +0800

    581. 最短无序连续子数组 官方题解

--- leetcode/algorithm/581-shortest-unsorted-continuous-subarray.js
+++ leetcode/algorithm/581-shortest-unsorted-continuous-subarray.js
@@ -29,3 +29,27 @@ var findUnsortedSubarray = function (nums) {
   result += len - start - 1;
   return len - result;
 };
+
+/**
+ * 官方题解 一次遍历
+ */
+var findUnsortedSubarray = function (nums) {
+  const n = nums.length;
+  let maxn = -Number.MAX_VALUE,
+    right = -1;
+  let minn = Number.MAX_VALUE,
+    left = -1;
+  for (let i = 0; i < n; i++) {
+    if (maxn > nums[i]) {
+      right = i;
+    } else {
+      maxn = nums[i];
+    }
+    if (minn < nums[n - i - 1]) {
+      left = n - i - 1;
+    } else {
+      minn = nums[n - i - 1];
+    }
+  }
+  return right === -1 ? 0 : right - left + 1;
+};
