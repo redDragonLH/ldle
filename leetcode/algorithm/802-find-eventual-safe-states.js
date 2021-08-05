48b7e823e60b47aa57959c4c2e1faedd35ef4710
liuhe <ok1332983774@163.com>
2021-08-05 10:52:48 +0800

    802. 找到最终的安全状态 官方题解 深度优先 + 三色标记法

--- leetcode/algorithm/802-find-eventual-safe-states.js
+++ leetcode/algorithm/802-find-eventual-safe-states.js
@@ -22,6 +22,62 @@ var eventualSafeNodes = function (graph) {
   // 从零开始遍历,用递归感觉增加耗时,不用就得用复杂结果保存中间过程
 };
 
-const deep = (graph, path, pathArr) => {
-    
+const deep = (graph, path, pathArr) => {};
+
+/**
+ * 官方题解 深度优先 + 三色标记法
+ *
+ * 若起始节点位于一个环内,或者能到达一个环,则该节点是不安全的,否则该节点是安全的(有环就不行,不仅要判断是否有符合题意的结果,还要判断有没有有环的分支)
+ *
+ * 可以使用深度优先搜索来找环,并在深度优先搜索时,用三种颜色对节点进行标记,规则如下:
+ *     * 白色(用 0 表示): 该节点尚未被访问
+ *     * 灰色(用 1 表示): 该节点位于递归栈中,或者在某个环上
+ *     * 黑色(用 2 表示): 该节点搜索完毕,是一个安全节点
+ *
+ *
+ * 首次访问一个节点时,将其标记为灰色,并继续搜索与其相连的节点
+ *
+ * 如果在搜索过程中遇到了一个灰色节点,则说明找到了一个环,此时退出搜索,栈中的节点仍保持为灰色,可以将「找到了环」这一信息传递到栈中的所有节点上
+ *
+ * 如果搜索过程中没有遇到灰色节点 则说明没有遇到环,那么递归返回前,将其标记由灰色改为黑色,即表示它是一个安全的节点
+ */
+var eventualSafeNodes = function (graph) {
+  const n = graph.length;
+  const color = new Array(n).fill(0);
+  const ans = [];
+  for (let i = 0; i < n; ++i) {
+    if (safe(graph, color, i)) {
+      ans.push(i);
+    }
+  }
+  return ans;
+};
+/**
+ * 代码很精巧,又返回值的同时也对数组进行了永久性的改变,使用了引用数据结构的特性
+ * 递归也减少了一些中间数据,减少了复杂度
+ * 
+ * 数据流设计合理
+ * @param {*} graph
+ * @param {*} color
+ * @param {*} x
+ * @returns
+ */
+// 确认节点是否安全
+const safe = (graph, color, x) => {
+  // 如果大于0 ,那说明已经处理过.直接返回
+  if (color[x] > 0) {
+    return color[x] === 2;
+  }
+  // 正在处理的节点
+  color[x] = 1;
+  // 获取下一级节点
+  for (const y of graph[x]) {
+    // 递归处理节点
+    if (!safe(graph, color, y)) {
+      return false;
+    }
+  }
+  // 没问题则置为2
+  color[x] = 2;
+  return true;
 };
