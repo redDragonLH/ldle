a006593ab133febf8b8a123b5146eb837d0ee116
liuhe <ok1332983774@163.com>
2021-09-02 09:56:59 +0800

    剑指 Offer 22. 链表中倒数第k个节点 双指针法

--- leetcode/algorithm/offer-22-lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.js
+++ leetcode/algorithm/offer-22-lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.js
@@ -29,7 +29,31 @@ var getKthFromEnd = function (head, k) {
 };
 /**
  * 增加了n的额外空间,但是因为只是保存的引用,所以问题不大
- * 
+ *
  * 执行用时：72 ms, 在所有 JavaScript 提交中击败了81.55%的用户
  * 内存消耗：39.2 MB, 在所有 JavaScript 提交中击败了38.29%的用户
+ */
+
+/**
+ * 双指针也可以解决,第一个指针正常遍历,在遍历k个之后第二个指针也进入,这样第一个指针到头之后第二个指针停在正好的位置上
+ */
+var getKthFromEnd = function (head, k) {
+  let fast = head,
+    slow = head;
+  let pos = 0;
+  while (fast) {
+    if (pos < k - 1) {
+      slow = slow.next;
+    }
+    fast = fast.next;
+    pos++;
+  }
+  return slow;
+};
+
+/**
+ * 双指针法
+ * 
+ * 执行用时：56 ms, 在所有 JavaScript 提交中击败了99.88%
+ * 的用户内存消耗：39.4 MB, 在所有 JavaScript 提交中击败了13.64%的用户
  */
