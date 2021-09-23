6133434644e1c6ece25cc4ebbfe82662a3b74dbe
liuhe <ok1332983774@163.com>
2021-09-23 10:15:12 +0800

    326. 3的幂 官方题解 判断是否为最大3的幂的约数

--- leetcode/algorithm/326-power-of-three.js
+++ leetcode/algorithm/326-power-of-three.js
@@ -10,14 +10,27 @@
  * @param {number} n
  * @return {boolean}
  */
- var isPowerOfThree = function (n) {
-    // 1是3的0次幂吧~~但是如果最后是1,则不是3的幂次方,这就得先单独处理输入,在用单独的函数递归
+var isPowerOfThree = function (n) {
+  // 1是3的0次幂吧~~但是如果最后是1,则不是3的幂次方,这就得先单独处理输入,在用单独的函数递归
   if (n === 1) return true;
   return recursion(n);
 };
 
-const recursion = (n)=>{
-    if (n < 3) return false;
-    if (n === 3) return true;
-    return recursion(n / 3); // 不能用parseInt,会导致数据误差
-}
+const recursion = (n) => {
+  if (n < 3) return false;
+  if (n === 3) return true;
+  return recursion(n / 3); // 不能用parseInt,会导致数据误差
+};
+
+/**
+ * 官方题解 判断是否为最大3的幂的约数
+ *
+ * 判断n是否为 3的19次方的约数即可
+ * 
+ * 在给定的32位有符号整数的范围内,最大的3的幂就是3的19次方
+ * 
+ * 约数: 整数a除以整数b(b != 0)除得的商正好是整数而没有余数
+ */
+var isPowerOfThree = function (n) {
+  return n > 0 && 1162261467 % n === 0;
+};
