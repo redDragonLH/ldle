00a9dec52f935e8ddee90d64ca83ee8f47860d5d
liuhe <ok1332983774@163.com>
2021-10-04 09:46:41 +0800

    482. 密钥格式化 官方题解：从后往前遍历

--- leetcode/algorithm/482-license-key-formatting.js
+++ leetcode/algorithm/482-license-key-formatting.js
@@ -43,3 +43,26 @@ var licenseKeyFormatting = function (s, k) {
   }
   return result.slice(0, result.length - 1);
 };
+
+/**
+ * 官方题解
+ */
+var licenseKeyFormatting = function (s, k) {
+  const ans = [];
+  let cnt = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] !== "-") {
+      cnt++;
+      ans.push(s[i].toUpperCase());
+      if (cnt % k === 0) {
+        ans.push("-");
+      }
+    }
+  }
+  if (ans.length > 0 && ans[ans.length - 1] === "-") {
+    ans.pop();
+  }
+
+  return ans.reverse().join("");
+};
