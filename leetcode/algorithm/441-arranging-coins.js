8871ba05ce18e2ba56f43de32e88be922ec46bb1
LH <ok1332983774@163.com>
2021-10-10 13:33:47 +0800

    441. 排列硬币 二分法

--- leetcode/algorithm/441-arranging-coins.js
+++ leetcode/algorithm/441-arranging-coins.js
@@ -11,10 +11,33 @@
  * @param {number} n
  * @return {number}
  */
- var arrangeCoins = function (n) {
+var arrangeCoins = function (n) {
     let line = 0;
     while (n > line) {
         n -= (++line)
     }
     return line
+};
+
+/**
+ * 官方题解：二分查找
+ * 
+ * 根据等差数列求和公式可知，前k个完整阶梯行所需的硬币数量为： total = (k × (k + 1))/2
+ * 
+ * 因此，可以通过二分查找计算 n 枚硬币可形成的完整阶梯行的总行数
+ * 
+ * 因为 1≤n≤(2^31)−1，所以 n 枚硬币至少可以组成 1 个完整阶梯行，至多可以组成 n 个完整阶梯行（在 n = 1 时得到）。
+ */
+
+var arrangeCoins = function (n) {
+    let left = 1, right = n;
+    while (left < right) {
+        const mid = Math.floor((right - left + 1) / 2) + left;
+        if (mid * (mid + 1) <= 2 * n) {
+            left = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
 };
