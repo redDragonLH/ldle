d07234ccfbc26537cd01d61cc1a98100e8a20759
liuhe <ok1332983774@163.com>
2021-10-12 10:31:08 +0800

    29. 两数相除 哪里有问题～～

--- leetcode/algorithm/29-divide-two-integers.js
+++ leetcode/algorithm/29-divide-two-integers.js
@@ -22,3 +22,45 @@ var divide = function (dividend, divisor) {
   }
   return result;
 };
+
+var divide = function (dividend, divisor) {
+  const MIN_VALUE = -2147483648;
+  const MAX_VALUE = 2147483648;
+  if (dividend == 0) return 0;
+  if (divisor == 1) return dividend;
+  if (divisor == -1) {
+    if (dividend > MIN_VALUE) return -dividend; // 只要不是最小的那个整数，都是直接返回相反数就好啦
+    return MIN_VALUE; // 是最小的那个，那就返回最大的整数啦
+  }
+  let a = dividend;
+  let b = divisor;
+  let sign = 1;
+  if ((a > 0 && b < 0) || (a < 0 && b > 0)) {
+    sign = -1;
+  }
+  a = a > 0 ? a : -a;
+  b = b > 0 ? b : -b;
+  let res = div(a, b);
+  if (sign > 0) return res > MAX_VALUE ? MAX_VALUE : res;
+  return -res;
+};
+let div = (a, b) => {
+  // 似乎精髓和难点就在于下面这几句
+  if (a < b) return 0;
+  let count = 1;
+  let tb = b; // 在后面的代码中不更新b
+  while (tb + tb <= a) {
+    count = count + count; // 最小解翻倍
+    tb = tb + tb; // 当前测试的值也翻倍
+  }
+  return count + div(a - tb, b);
+};
+
+/**
+ * -2147483648
+ * -1
+ *
+ * 输出: -2147483648
+ *
+ * 预期: 2147483647
+ */
