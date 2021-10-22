b6351047efbd3b43d940f41c17c150489fb254ff
liuhe <ok1332983774@163.com>
2021-10-22 10:47:36 +0800

    229. 求众数 II 摩尔投票法

--- leetcode/README.md
+++ leetcode/README.md
@@ -13,3 +13,5 @@
 **拒绝采样 算法**
 
 **字典树**[208. 实现 Trie (前缀树) 的官方题解](https://leetcode-cn.com/problems/implement-trie-prefix-tree/solution/shi-xian-trie-qian-zhui-shu-by-leetcode-ti500/)
+
+**摩尔投票法**
--- leetcode/algorithm/229-majority-element-ii.js
+++ leetcode/algorithm/229-majority-element-ii.js
@@ -29,3 +29,55 @@ var majorityElement = function (nums) {
  * 执行用时：88 ms, 在所有 JavaScript 提交中击败了22.10%的用户
  * 内存消耗：40.3 MB, 在所有 JavaScript 提交中击败了81.77%的用户
  */
+
+/**
+ * 摩尔投票法
+ * 
+ * 内容: 摩尔投票法的核心思想为对拼消耗
+ *  核心思想基于这样的事实逻辑:
+ *      *   每次从序列里选择两个不相同的数字删掉(或称为「抵消」),最后剩下一个数字或几个相同的数字,就是出现次数大于总数一半的那个元素,
+ */
+ var majorityElement = function(nums) {
+    let element1 = 0;
+    let element2 = 0;
+    let vote1 = 0;
+    let vote2 = 0;
+
+    for (const num of nums) {
+        if (vote1 > 0 && num === element1) { //如果该元素为第一个元素，则计数加1
+            vote1++;
+        } else if (vote2 > 0 && num === element2) { //如果该元素为第二个元素，则计数加1
+            vote2++;
+        } else if (vote1 === 0) { // 选择第一个元素
+            element1 = num;
+            vote1++;
+        } else if (vote2 === 0) { // 选择第二个元素
+            element2 = num;
+            vote2++;
+        } else { //如果三个元素均不相同，则相互抵消1次
+            vote1--;
+            vote2--;
+        }
+    }
+
+    let cnt1 = 0;
+    let cnt2 = 0;
+    for (const num of nums) {
+        if (vote1 > 0 && num === element1) {
+            cnt1++;
+        }
+        if (vote2 > 0 && num === element2) {
+            cnt2++;
+        }
+    }
+    // 检测元素出现的次数是否满足要求
+    const ans = [];
+    if (vote1 > 0 && cnt1 > Math.floor(nums.length / 3)) {
+        ans.push(element1);
+    }
+    if (vote2 > 0 && cnt2 > Math.floor(nums.length / 3)) {
+        ans.push(element2);
+    }
+
+    return ans;
+};
