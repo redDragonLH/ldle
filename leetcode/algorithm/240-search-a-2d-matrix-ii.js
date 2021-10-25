e9aae4c7569ddc6edd0228cf220dff4416d61911
liuhe <ok1332983774@163.com>
2021-10-25 10:53:56 +0800

    240. 搜索二维矩阵 II Z字形查找

--- leetcode/algorithm/240-search-a-2d-matrix-ii.js
+++ leetcode/algorithm/240-search-a-2d-matrix-ii.js
@@ -54,3 +54,33 @@ const search = (nums, target) => {
   }
   return -1;
 };
+
+/**
+ * 官方题解: Z字形查找
+ * 可以从矩阵 matrix 的右上角(0,n-1)进行搜索.在每一步搜索过程中,如果我们位于位置(x,y),那么希望再以矩阵的左下角为左下角(n-1,0)、以(行,y)为右上角
+ * 的矩阵中进行搜索,即行的范围为[x,m-1],列的范围为[0,y]:
+ *      * 如果 matrix[x,y] = target,说明搜索完成
+ *      * 如果 matrix[x,y] > target,由于每一列都是升序排列,那么在当前的搜索矩阵中,所有位于第y列的元素都是严格大于 target,因此
+ *          可以将它们全部忽略,即将y减少1;
+ *      * 如果 matrix[x,y] < target, 由于每一行的元素都是升序排列的,那么在当前的搜索矩阵中,所有位于第x行的元素都是严格小于taret的,
+ * 因此可以将它们全部忽略,即将x增加1
+ *
+ * 如果搜索过程中超出了矩阵的边界,说明矩阵中不存在 target.
+ */
+var searchMatrix = function (matrix, target) {
+  const m = matrix.length,
+    n = matrix[0].length;
+  let x = 0,
+    y = n - 1;
+  while (x < m && y >= 0) {
+    if (matrix[x][y] === target) {
+      return true;
+    }
+    if (matrix[x][y] > target) {
+      --y;
+    } else {
+      ++x;
+    }
+  }
+  return false;
+};
