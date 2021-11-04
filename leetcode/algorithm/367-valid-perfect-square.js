a05e17c67e60432b0b9d9e78b8a342998de7925a
liuhe <ok1332983774@163.com>
2021-11-04 09:14:06 +0800

    367. 有效的完全平方数 暴力遍历

--- leetcode/algorithm/367-valid-perfect-square.js
+++ leetcode/algorithm/367-valid-perfect-square.js
@@ -9,8 +9,24 @@
  * @param {number} num
  * @return {boolean}
  */
- var isPerfectSquare = function (num) {
-    let sqrtNum = Math.floor(Math.sqrt(num));
-    // 必须进行 乘法运算验证才行
-    return sqrtNum * sqrtNum === num;
-  };
+var isPerfectSquare = function (num) {
+  let sqrtNum = Math.floor(Math.sqrt(num));
+  // 必须进行 乘法运算验证才行
+  return sqrtNum * sqrtNum === num;
+};
+
+/**
+ * 可以使用暴力法遍历所有数据找到 x*x = num的值
+ */
+var isPerfectSquare = function (num) {
+  let x = 1;
+  while (x * x < num) {
+    x++;
+  }
+  if (x * x === num) return true;
+  return false;
+};
+/**
+ * 执行用时：60 ms, 在所有 JavaScript 提交中击败了96.11%的用户
+ * 内存消耗：37.6 MB, 在所有 JavaScript 提交中击败了67.25%的用户
+ */
