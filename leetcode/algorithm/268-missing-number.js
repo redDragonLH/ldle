3ef2d04b7f543dd323d892a78184676dd0e839f5
LH <ok1332983774@163.com>
2021-11-06 00:40:15 +0800

    268. 丢失的数字 使用数组元素数量判断，异或法

--- leetcode/algorithm/268-missing-number.js
+++ leetcode/algorithm/268-missing-number.js
@@ -21,3 +21,26 @@ var missingNumber = function (nums) {
  * 执行用时：68 ms, 在所有 JavaScript 提交中击败了95.00%的用户
  * 内存消耗：40.4 MB, 在所有 JavaScript 提交中击败了27.85%的用户
  */
+
+/**
+ * 因为有隐含的元素数量这一数据，所以也可以使用异或元素与i,剩余的就是缺失的元素
+ * 
+ * 异或：二进制位置相同的数据为false/0,不同的数据为true/1
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    let len = nums.length;
+    let result = 0;
+    for (let i = 0; i <= len; i++) {
+        result ^= i;
+        result ^= (nums[i] || 0);
+
+    }
+    return result;
+};
+/**
+ * 虽然使用的原理非常基础与深奥，但是效率并不是很高
+ * 执行用时：80 ms, 在所有 JavaScript 提交中击败了66.62%的用户
+ * 内存消耗：40.1 MB, 在所有 JavaScript 提交中击败了70.15%的用户
+ */
