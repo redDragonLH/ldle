e34c63d64abe4276b757cf3ddde7a75e7a8ad46e
LH <ok1332983774@163.com>
2021-11-07 23:18:48 +0800

    598. 范围求和 II 推理

--- leetcode/algorithm/598-range-addition-ii.js
+++ leetcode/algorithm/598-range-addition-ii.js
@@ -18,3 +18,27 @@
 var maxCount = function (m, n, ops) {
 
 };
+
+/**
+ * 
+ * 其实所有的数据都是从左上角开始的，不管数是多少，那么这样子的话 （0，0）肯定是最大的，那么边界是哪里呢，
+ * 理论上就是步骤里面 最小的a,和最小的b
+ * 
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function (m, n, ops) {
+    if (!ops.length) return m * n;
+    let min = [40000, 40000];
+    ops.forEach(e => {
+        min[0] = Math.min(min[0], e[0])
+        min[1] = Math.min(min[1], e[1])
+    })
+    return min[0] * min[1]
+};
+/**
+ * 执行用时：76 ms, 在所有 JavaScript 提交中击败了49.57%的用户
+ * 内存消耗：39.3 MB, 在所有 JavaScript 提交中击败了87.18%的用户
+ */
