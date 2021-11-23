2161087c6a9633f9dc406cd98758b92022ebdf7e
liuhe <ok1332983774@163.com>
2021-11-23 09:51:17 +0800

    859. 亲密字符串 自题解

---
+++ leetcode/algorithm/859-buddy-strings.js
@@ -0,0 +1,47 @@
+/**
+ * 859. 亲密字符串
+ *
+ * 给你两个字符串 s 和 goal ，只要我们可以通过交换 s 中的两个字母得到与 goal 相等的结果，就返回 true ；否则返回 false 。
+ * 交换字母的定义是：取两个下标 i 和 j （下标从 0 开始）且满足 i != j ，接着交换 s[i] 和 s[j] 处的字符。
+ * 例如，在 "abcd" 中交换下标 0 和下标 2 的元素可以生成 "cbad" 。
+ */
+
+/**
+ * 也就是说,两个字符串只能有两个位置的字符不一样,且字符内容数量是一样的
+ * 
+ * 让特殊用例搞爆炸,这个思路还能优化
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function (s, goal) {
+  let dislocation = [];
+  let sLen = s.length;
+  if (sLen !== goal.length) return false;
+  for (let i = 0; i < sLen; i++) {
+    if (s[i] !== goal[i]) dislocation.push(i);
+  }
+  if (dislocation.length === 0) {
+    // 还要考虑相等但是能交换字符的情况
+    // 也就是一个字符串里面必须有重复字符
+    let letters = new Array(26).fill(0);
+    const aCode = 97;
+    for (let i = 0; i < sLen; i++) {
+      let iCode = s[i].charCodeAt() - aCode;
+      letters[iCode]++;
+      if (letters[iCode] >= 2) return true;
+    }
+    return false;
+  } else if (
+    dislocation.length === 2 &&
+    s[dislocation[0]] === goal[dislocation[1]] &&
+    s[dislocation[1]] === goal[dislocation[0]]
+  ) {
+    return true;
+  } else return false;
+};
+/**
+ * 超慢
+ * 执行用时：116 ms, 在所有 JavaScript 提交中击败了5.85%的用户
+ * 内存消耗：39.4 MB, 在所有 JavaScript 提交中击败了62.44%的用户
+ */
