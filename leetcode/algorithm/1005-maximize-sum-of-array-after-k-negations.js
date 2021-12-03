e32139919c5068dd5ecec23cffd1be60db2f8609
liuhe <ok1332983774@163.com>
2021-12-03 10:11:38 +0800

    1005. K 次取反后最大化的数组和 官方题解

--- leetcode/algorithm/1005-maximize-sum-of-array-after-k-negations.js
+++ leetcode/algorithm/1005-maximize-sum-of-array-after-k-negations.js
@@ -43,3 +43,51 @@ var largestSumAfterKNegations = function (nums, k) {
 
   return nums.reduce((p, c) => p + c, 0); //补全逻辑
 };
+
+/**
+ * 因为可以重复选择下标,那么其实就只有两种状态,选择1,2,而奇数k必定有个选择取反1次,取反两次等于没取反,
+ * 那就看看有多少小于0的数,能支持一次取反,如果剩下的k是偶数则不必再处理,是奇数的话就得找到最小的数取反,
+ *
+ * 小顶堆~
+ */
+
+/**
+ * 官方题解
+ */
+ var largestSumAfterKNegations = function(nums, k) {
+    const freq = new Map();
+    // 转为map数据
+    for (const num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+    // 获取当前的总和吗?
+    let ans = _.sum(nums);
+    // 暂时只处理负数
+    for (let i = -100; i < 0; ++i) {
+        if (freq.has(i)) {
+            const ops = Math.min(k, freq.get(i));
+            ans += (-i) * ops * 2;
+            freq.set(i, freq.get(i) - ops);
+            freq.set(-i, (freq.get(-i) || 0) + ops);
+            k -= ops;
+            if (k === 0) {
+                break;
+            }
+        }
+    }
+    // 然后就是检查k是奇数的情况
+    if (k > 0 && k % 2 === 1 && !freq.has(0)) {
+        for (let i = 1; i <= 100; ++i) {
+            if (freq.has(i)) {
+                ans -= i * 2;
+                break;
+            }
+        }
+    }
+    return ans;
+};
+/**
+ * 优化点:
+ *  1. 循环要比我的少,只有100个,
+ *  2. 减少了排序
+ */
