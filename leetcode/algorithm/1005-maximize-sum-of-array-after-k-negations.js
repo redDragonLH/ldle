015f32919e084780a867cf107882de0a92b1b192
liuhe <ok1332983774@163.com>
2021-12-03 09:46:31 +0800

    1005. K 次取反后最大化的数组和 分支陷阱 优化失败 超时

--- leetcode/algorithm/1005-maximize-sum-of-array-after-k-negations.js
+++ leetcode/algorithm/1005-maximize-sum-of-array-after-k-negations.js
@@ -11,8 +11,10 @@
 
 /**
  * 感觉像是找寻最小值
- * 
+ *
  * 这种分支太多了,非常容易陷进分支陷阱,一直加特殊处理分支
+ *
+ * 超时~
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
@@ -22,13 +24,22 @@ var largestSumAfterKNegations = function (nums, k) {
   let index = 0;
   while (k) {
     if (nums[index] < 0) {
+      nums[index] = -nums[index];
       index++;
       k--;
-    } else if (nums[index] > 0 && !(k % 2)) {
+    } else if (nums[index] >= 0 && !(k % 2)) {
       return nums.reduce((p, c) => p + c, 0);
-    } else if (nums[index] > 0 && k % 2) {
-      nums[0] = -nums[index];
+    } else if (nums[index] >= 0 && k % 2) {
+      // 到这个阶段的时候由于必须要翻转一个,那么就得找到最小的数据,而由于前边得到元素已经翻转
+      // 所以当前数据表示为图形是一个两侧高中间低的低谷,而最小值就是当前的元素和前一个已翻转元素的最小的一个
+      if (index === 0 || nums[index - 1] > nums[index]) {
+        nums[index] = -nums[index];
+      } else {
+        nums[index - 1] = -nums[index - 1];
+      }
       return nums.reduce((p, c) => p + c, 0);
     }
   }
+
+  return nums.reduce((p, c) => p + c, 0); //补全逻辑
 };
