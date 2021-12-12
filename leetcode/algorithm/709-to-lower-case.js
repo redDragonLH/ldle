cae22847b36dc0eb17622eaf7a317f746274761a
LH <ok1332983774@163.com>
2021-12-12 23:42:41 +0800

    709. 转换成小写字母

---
+++ leetcode/algorithm/709-to-lower-case.js
@@ -0,0 +1,13 @@
+/**
+ * 709. 转换成小写字母
+ * 
+ * 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+    return s.toLocaleLowerCase();
+};
