7678c6045a26f5b1464cd0e830ba335cca03482f
liuhe <ok1332983774@163.com>
2021-12-27 10:20:21 +0800

    825. 适龄的朋友 排序+双指针： 失败

---
+++ leetcode/algorithm/825-friends-of-appropriate-ages.js
@@ -0,0 +1,39 @@
+/**
+ * 825. 适龄的朋友
+ *
+ * 在社交媒体网站上有 n 个用户。给你一个整数数组 ages ，其中 ages[i] 是第 i 个用户的年龄。
+ *
+ * 如果下述任意一个条件为真，那么用户 x 将不会向用户 y（x != y）发送好友请求：
+ *  * age[y] <= 0.5 * age[x] + 7
+ *  * age[y] > age[x]
+ *  * age[y] > 100 && age[x] < 100
+ *
+ * 否则，x 将会向 y 发送一条好友请求。
+ *
+ * 注意，如果 x 向 y 发送一条好友请求，y 不必也向 x 发送一条好友请求。另外，用户不会向自己发送好友请求。
+ *
+ * 返回在该社交媒体网站上产生的好友请求总数。
+ */
+
+/**
+ * 他要对所有人进行check,除了自己,这循环量得多大~~
+ * 
+ * 超时
+ * @param {number[]} ages
+ * @return {number}
+ */
+var numFriendRequests = function (ages) {
+  let result = 0;
+  let len = ages.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (!(ages[j] <= 0.5 * ages[i] + 7 || ages[j] > ages[i])) {
+        result++;
+      }
+      if (!(ages[i] <= 0.5 * ages[j] + 7 || ages[i] > ages[j])) {
+        result++;
+      }
+    }
+  }
+  return result;
+};
