dfbf34c47b8eaa6ff14c006bbe1c00799f876aba
liuhe <ok1332983774@163.com>
2021-12-30 09:40:20 +0800

    846. 一手顺子 官方：贪心

--- leetcode/algorithm/846-hand-of-straights.js
+++ leetcode/algorithm/846-hand-of-straights.js
@@ -27,3 +27,35 @@ var isNStraightHand = function (hand, groupSize) {
     len--;
   }
 };
+
+/**
+ * 官方题解: 贪心
+ */
+var isNStraightHand = function (hand, groupSize) {
+  const n = hand.length;
+  if (n % groupSize !== 0) {
+    return false;
+  }
+  hand.sort((a, b) => a - b);
+  const cnt = new Map();
+  for (const x of hand) {
+      //相同元素的数量
+    cnt.set(x, (cnt.get(x) || 0) + 1);
+  }
+  for (const x of hand) {
+    if (!cnt.has(x)) {
+      continue;
+    }
+    for (let j = 0; j < groupSize; j++) {
+      const num = x + j;
+      if (!cnt.has(num)) {
+        return false;
+      }
+      cnt.set(num, cnt.get(num) - 1);
+      if (cnt.get(num) == 0) {
+        cnt.delete(num);
+      }
+    }
+  }
+  return true;
+};
