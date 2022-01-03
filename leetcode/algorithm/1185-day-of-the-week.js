15a623ffc2ec05f6ce7133ca5eb1880bec282502
LH <ok1332983774@163.com>
2022-01-03 18:20:30 +0800

    1185. 一周中的第几天

--- leetcode/algorithm/1185-day-of-the-week.js
+++ leetcode/algorithm/1185-day-of-the-week.js
@@ -1,5 +1,5 @@
 /**
- * 1185. 一周中的第几天
+ * 
  * 
  * 给你一个日期，请你设计一个算法来判断它是对应一周中的哪一天。
  * 输入为三个整数：day、month 和 year，分别表示日、月、年。
@@ -32,3 +32,7 @@ var dayOfTheWeek = function (day, month, year) {
     days += day;
     return week[(days + 3) % 7];
 }
+
+/**
+ * 也可以用自带的API~~
+ */
