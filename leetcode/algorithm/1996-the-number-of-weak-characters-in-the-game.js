22c28f836ca94183816d32de1edc84d3d973011b
liuhe <ok1332983774@163.com>
2022-01-28 09:53:01 +0800

    1996. 游戏中弱角色的数量 单调栈

--- leetcode/algorithm/1996-the-number-of-weak-characters-in-the-game.js
+++ leetcode/algorithm/1996-the-number-of-weak-characters-in-the-game.js
@@ -25,7 +25,7 @@ var numberOfWeakCharacters = function (properties) {
   // 那得有个数据结构保存比当前元素大的
   let maxDef = 0;
   for (const p of properties) {
-      // 把 对比数字 返回到较小的情况,避免对比数字过大导致遗漏
+    // 把 对比数字 返回到较小的情况,避免对比数字过大导致遗漏
     if (p[1] < maxDef) {
       result++;
     } else {
@@ -33,4 +33,30 @@ var numberOfWeakCharacters = function (properties) {
     }
   }
   return result;
+};
+
+/**
+ * 官方题解: 单调栈
+ *
+ *  使用单调递增栈的解法,单调递增栈中保证栈内所有的元素从小到大排列.按照角色攻击值的大小从低到高依次遍历每个元素,使用单调
+ * 递增栈保存所有角色的防御值,遍历时如果发现栈顶的角色p的防御值小于当前的角色q的防御值,则可以认为找到攻击值和防御值都严格
+ * 大于p的角色q
+ *  如果所有角色的攻击值都不相同,则上述的单调递增栈解法比较简单,难点在于如何处理攻击值相同但防御值不同的角色比较问题.按照攻击值
+ * 相同的防御值从大到小排列,这样即可以保证攻击值相同但防御值不同时的角色在进行比较时不会产生计数
+ */
+var numberOfWeakCharacters = function (properties) {
+  //按照攻击值相同的防御值从大到小排列
+  properties.sort((o1, o2) => {
+    return o1[0] === o2[0] ? o2[1] - o1[1] : o1[0] - o2[0];
+  });
+  let ans = 0;
+  const st = [];
+  for (const p of properties) {
+    while (st.length && st[st.length - 1] < p[1]) {
+      st.pop();
+      ans++;
+    }
+    st.push(p[1]);
+  }
+  return ans;
 };
