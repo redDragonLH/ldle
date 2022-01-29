16118ea59e39b61e400508b0d25f59cf2746567d
LH <ok1332983774@163.com>
2022-01-30 07:47:36 +0800

    884. 两句话中的不常见单词 遍历 成功

--- leetcode/algorithm/884-uncommon-words-from-two-sentences.js
+++ leetcode/algorithm/884-uncommon-words-from-two-sentences.js
@@ -17,19 +17,42 @@
 var uncommonFromSentences = function (s1, s2) {
     let data = new Set();
     let result = [];
-    let invalid =new Set()
+    let invalid = new Set()
     for (const item of s1.split(' ')) {
-        if(data.has(item)){
+        if (data.has(item)) {
             invalid.add(item)
             data.delete(item)
-        }else {
+        } else {
             data.add(item)
         }
     }
     for (const item of s2.split(' ')) {
-        if(!invalid.has(item)&&!data.has(item))result.push(item)
+        if (!invalid.has(item) && !data.has(item)) result.push(item)
         else data.delete(item)
     }
     result.push(...data.entries)
     return result;
+};
+
+/**
+ * 由于那边都可能会有重复数据，所以放在一起处理在代码上会简单一点，但是由于会有
+ * 出现两次以上的情况，单纯的删掉也有些问题，需要一个变量来保存被删除数据，
+ * 如果用Map的话应该就不需要了，多的就放key里
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+var uncommonFromSentences = function (s1, s2) {
+    let strArr = (s1 + ' ' + s2).split(' ')
+    let result = new Set();
+    let deleteData = new Set();
+    for (const item of strArr) {
+        if (result.has(item)) {
+            result.delete(item)
+            deleteData.add(item)
+        } else if (!deleteData.has(item)) {
+            result.add(item)
+        }
+    }
+    return new Array(...result);
 };
