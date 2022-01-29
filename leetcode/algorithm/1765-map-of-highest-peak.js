48c3ed45a2aae3863f7415024156858ad5812c59
liuhe <ok1332983774@163.com>
2022-01-29 09:43:37 +0800

    1765. 地图中的最高点

---
+++ leetcode/algorithm/1765-map-of-highest-peak.js
@@ -0,0 +1,67 @@
+/**
+ * 1765. 地图中的最高点
+ *
+ * 给你一个大小为 m x n 的整数矩阵 isWater ，它代表了一个由 陆地 和 水域 单元格组成的地图。
+ *  * 如果 isWater[i][j] == 0 ，格子 (i, j) 是一个 陆地 格子。
+ *  * 如果 isWater[i][j] == 1 ，格子 (i, j) 是一个 水域 格子。
+ *
+ * 你需要按照如下规则给每个单元格安排高度：
+ *  * 每个格子的高度都必须是非负的。
+ *  * 如果一个格子是是 水域 ，那么它的高度必须为 0 。
+ *  * 任意相邻的格子高度差 至多 为 1 。当两个格子在正东、南、西、北方向上相互紧挨着，就称它们为相邻的格子。（也就是说它们有一条公共边）
+ *
+ * 找到一种安排高度的方案，使得矩阵中的最高高度值 最大 。
+ *
+ * 请你返回一个大小为 m x n 的整数矩阵 height ，其中 height[i][j] 是格子 (i, j) 的高度。如果有多种解法，请返回 任意一个 。
+ */
+
+/**
+ * 也就是找到水域,然后往四周扩散
+ *
+ * 多源广度优先搜索~~~
+ * 
+ * 由java代码转译而来
+ * 
+ * 超时~~
+ * @param {number[][]} isWater
+ * @return {number[][]}
+ */
+var highestPeak = function (isWater) {
+  let dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+
+  let m = isWater.length,
+    n = isWater[0].length;
+  let ans = Array.from({ length: m }, () => Array.from({ length: n }));
+
+  for (let i = 0; i < m; ++i) {
+    ans[i].fill(-1); // -1 表示该格子尚未被访问过
+    // Arrays.fill(ans[i], -1);
+  }
+
+  let queue = [];
+  for (let i = 0; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
+      if (isWater[i][j] == 1) {
+        ans[i][j] = 0;
+        queue.push([i, j]); // 将所有水域入队
+      }
+    }
+  }
+  while (queue.length) {
+    let p = queue.shift();
+    for (let dir of dirs) {
+      let x = p[0] + dir[0],
+        y = p[1] + dir[1];
+      if (0 <= x && x < m && 0 <= y && y < n && ans[x][y] == -1) {
+        ans[x][y] = ans[p[0]][p[1]] + 1;
+        queue.push([x, y]);
+      }
+    }
+  }
+  return ans;
+};
