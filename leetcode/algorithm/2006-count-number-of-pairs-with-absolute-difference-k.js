e1a924357d4ba8d35b7c8e9ed233707c2ac3d753
liuhe <ok1332983774@163.com>
2022-02-09 09:06:55 +0800

    2006. 差的绝对值为 K 的数对数目 暴力

---
+++ leetcode/algorithm/2006-count-number-of-pairs-with-absolute-difference-k.js
@@ -0,0 +1,15 @@
+/**
+ * 2006. 差的绝对值为 K 的数对数目
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回数对 (i, j) 的数目，满足 i < j 且 |nums[i] - nums[j]| == k 。
+ *
+ * |x| 的值定义为：
+ *  * 如果 x >= 0 ，那么值为 x 。
+ *  * 如果 x < 0 ，那么值为 -x 。
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function (nums, k) {};
