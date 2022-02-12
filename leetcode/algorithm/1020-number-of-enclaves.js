5bc83339dd718f859853161209010af3f1f2d834
LH <ok1332983774@163.com>
2022-02-12 10:07:11 +0800

    1020. 飞地的数量 广度优先 超时

---
+++ leetcode/algorithm/1020-number-of-enclaves.js
@@ -0,0 +1,59 @@
+/**
+ * 1020. 飞地的数量
+ * 
+ * 给你一个大小为 m x n 的二进制矩阵 grid ，其中 0 表示一个海洋单元格、1 表示一个陆地单元格。
+ * 
+ * 一次 移动 是指从一个陆地单元格走到另一个相邻（上、下、左、右）的陆地单元格或跨过 grid 的边界。
+ * 
+ * 返回网格中 无法 在任意次数的移动中离开网格边界的陆地单元格的数量。
+ */
+/**
+ * 思路：广度优先遍历四周，把能够到达边界的块置为别的数，然后遍历整个矩阵，获得1的数量
+ *      但是遍历四周逻辑上是否过于复杂，是否有更简单的思路
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var numEnclaves = function (grid) {
+    let m = grid.length, n = grid[0].length;
+    let fourSide = [
+        [0, n], [m - 1, n], [0, m], [n - 1, m]
+    ]
+    fourSide.forEach((item, index) => {
+        for (let i = 0; i < item[1]; i++) {
+            if (index > 1) {
+                if (grid[i][item[0]] === 1) breadth([i, item[0]], grid, m)
+            } else {
+                if (grid[item[0]][i] === 1) breadth([item[0], i], grid, m)
+            }
+        }
+    })
+    let result = 0
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            grid[i][j] === 1 && result++
+
+        }
+    }
+    return result
+};
+const breadth = (point, grid, m) => {
+    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    let area = [[point[0], point[1]]]
+    while (area.length) {
+        let len = area.length
+        while (len--) {
+            let data = area.shift()
+            grid[data[0]][data[1]] = 2
+            for (const dir of dirs) {
+                if (data[0] + dir[0] > -1 && data[0] + dir[0] < m) {
+                    grid[data[0] + dir[0]][data[1] + dir[1]] === 1 && area.push([data[0] + dir[0], data[1] + dir[1]]);
+
+                }
+            }
+        }
+    }
+}
+/**
+ * 超时
+ * 哪里还能优化，还是思路就有问题
+ */
