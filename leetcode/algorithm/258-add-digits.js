d4c31004b33bf51292e910a1a6ced1495741c0c7
LH <ok1332983774@163.com>
2022-03-03 08:07:37 +0800

    258. 各位相加 树根性值

--- leetcode/algorithm/258-add-digits.js
+++ leetcode/algorithm/258-add-digits.js
@@ -23,3 +23,16 @@ var addDigits = function (num) {
  * 执行用时：72 ms, 在所有 JavaScript 提交中击败了81.66%的用户
  * 内存消耗：42.5 MB, 在所有 JavaScript 提交中击败了19.16%的用户
  */
+
+/**
+ * 此题本质是计算自然数的 树根。
+ * 
+ * 树根： 又称数字根，是自然数的一种性值，每个自然数都有一个树根。对于给定的自然数，
+ *      反复将个位上的数字相加，直到结果为一位数，则该一位数即为原自然数的树根
+ * 数学性值有数学公式
+ * @param {number} num
+ * @return {number}
+ */
+ var addDigits = function(num) {
+    return (num - 1) % 9 + 1;
+};
