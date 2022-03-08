939a3f2732fb174a42af46e1d161f78104272670
LH <ok1332983774@163.com>
2022-03-08 08:57:12 +0800

    2055. 蜡烛之间的盘子 官方题解 前缀和

--- leetcode/algorithm/2055-plates-between-candles.js
+++ leetcode/algorithm/2055-plates-between-candles.js
@@ -50,3 +50,41 @@ const findDish = (s, left, right) => {
 /**
  * 超时
  */
+
+/**
+ * 官方题解 前缀和
+ */
+var platesBetweenCandles = function (s, queries) {
+  const n = s.length;
+  const preSum = new Array(n).fill(0);
+  // 处理盘子数量这样计算盘子数量时只要 preSum_y - preSum_(x-1)
+
+  for (let i = 0, sum = 0; i < n; i++) {
+    if (s[i] === '*') {
+      sum++;
+    }
+    preSum[i] = sum;
+  }
+  // 左右节点也要前缀处理啊
+  const left = new Array(n).fill(0);;
+  for (let i = 0, l = -1; i < n; i++) {
+    if (s[i] === '|') {
+      l = i;
+    }
+    left[i] = l;
+  }
+  const right = new Array(n).fill(0);;
+  for (let i = n - 1, r = -1; i >= 0; i--) {
+    if (s[i] === '|') {
+      r = i;
+    }
+    right[i] = r;
+  }
+  const ans = new Array(queries.length).fill(0);
+  for (let i = 0; i < queries.length; i++) {
+    const query = queries[i];
+    const x = right[query[0]], y = left[query[1]];
+    ans[i] = x === -1 || y === -1 || x >= y ? 0 : preSum[y] - preSum[x];
+  }
+  return ans;
+};
