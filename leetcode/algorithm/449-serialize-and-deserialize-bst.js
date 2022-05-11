f9ac511b23ea2e83bf95903c849371305c00d69f
liuhe <ok1332983774@163.com>
2022-05-11 10:23:10 +0800

    449. 序列化和反序列化二叉搜索树 官方

--- leetcode/algorithm/449-serialize-and-deserialize-bst.js
+++ leetcode/algorithm/449-serialize-and-deserialize-bst.js
@@ -39,3 +39,58 @@ var deserialize = function (data) {
  * Your functions will be called as such:
  * deserialize(serialize(root));
  */
+
+/**
+ * 转成三种遍历方式都可以,只要两个方法的处理顺序是一样的就可以
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+    let result = [];
+    deep(root, result)
+    return result.join(',')
+};
+const deep = (root, array) => {
+    if (!root) return false
+    deep(root.left, array)
+    deep(root.right, array)
+    array.push(root.val)
+}
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function (data) {
+    if (data.length === 0) {
+        return null;
+    }
+    let arr = data.split(',');
+    const length = arr.length;
+    const stack = [];
+
+    // 字符串转数字
+    for (let i = 0; i < length; i++) {
+        stack.push(parseInt(arr[i]));
+    }
+
+    const construct = (lower, upper, stack) => {
+        if (stack.length === 0 || stack[stack.length - 1] < lower || stack[stack.length - 1] > upper) {
+            return null;
+        }
+        const val = stack.pop();
+        const root = new TreeNode(val);
+        root.right = construct(val, upper, stack);
+        root.left = construct(lower, val, stack);
+        return root;
+    }
+
+    return construct(-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, stack);
+};
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
