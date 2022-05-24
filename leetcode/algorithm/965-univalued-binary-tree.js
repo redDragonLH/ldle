dd9219debf270fb6cd4308d2b8e025058cdf6d14
liuhe <ok1332983774@163.com>
2022-05-24 09:23:19 +0800

    965. 单值二叉树 第三方题解： 广度优先优化

--- leetcode/algorithm/965-univalued-binary-tree.js
+++ leetcode/algorithm/965-univalued-binary-tree.js
@@ -40,3 +40,33 @@ var isUnivalTree = function (root) {
  * 执行用时：72 ms, 在所有 JavaScript 提交中击败了8.96%的用户
  * 内存消耗：41.6 MB, 在所有 JavaScript 提交中击败了40.09%的用户
  */
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 第三方题解
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isUnivalTree = function (root) {
+     // 标志值抽出来,不再用 root.val
+    const value = root.val
+    let queue = []
+    queue.push(root)
+    while (queue.length) {
+        const node = queue.shift()
+        if (node.val != value) {
+            return false
+        }
+        node.left ? queue.push(node.left) : ""
+        node.right ? queue.push(node.right) : ""
+    }
+    return true
+};
