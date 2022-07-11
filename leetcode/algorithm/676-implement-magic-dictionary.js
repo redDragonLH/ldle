fd58d354f3693d2e7899250cec5822299b0d3366
liuhe <ok1332983774@163.com>
2022-07-11 10:10:51 +0800

    676. 实现一个魔法字典 官方题解：字典树

--- leetcode/algorithm/676-implement-magic-dictionary.js
+++ leetcode/algorithm/676-implement-magic-dictionary.js
@@ -34,3 +34,83 @@ MagicDictionary.prototype.search = function (searchWord) {};
  * obj.buildDict(dictionary)
  * var param_2 = obj.search(searchWord)
  */
+
+/**
+ * 优化算法
+ *
+ */
+var MagicDictionary = function () {
+  this.root = new Trie();
+};
+/**
+ * 构建很简单
+ * @param {string[]} dictionary 
+ */
+MagicDictionary.prototype.buildDict = function (dictionary) {
+  for (const word of dictionary) {
+    let cur = this.root;
+    for (let i = 0; i < word.length; ++i) {
+      const ch = word[i];
+      // 转成数字这样多转一遍是否有意义
+      const idx = ch.charCodeAt() - "a".charCodeAt();
+      if (!cur.child[idx]) {
+        cur.child[idx] = new Trie();
+      }
+      cur = cur.child[idx];
+    }
+    cur.isFinished = true;
+  }
+};
+
+/**
+ * 如果保存结构是字典树的话,搜索算法就有点复杂,不过也没有那么负责,按层查呗,也是对不上的层就拿一个变量标记+1,有第二个对不上的就直接跳过,
+ * 结束也直接查最后一个字典树对应的字母是不是终结字符
+ * @param {string} searchWord 
+ * @returns 
+ */
+MagicDictionary.prototype.search = function (searchWord) {
+  return dfs(searchWord, this.root, 0, false);
+};
+/**
+ * 
+ * @param {*} searchWord 
+ * @param {*} node 
+ * @param {*} pos 
+ * @param {*} modified 
+ * @returns 
+ */
+const dfs = (searchWord, node, pos, modified) => {
+  if (pos === searchWord.length) {
+    return modified && node.isFinished;
+  }
+  let idx = searchWord[pos].charCodeAt() - "a".charCodeAt();
+  if (node.child[idx]) {
+    if (dfs(searchWord, node.child[idx], pos + 1, modified)) {
+      return true;
+    }
+  }
+  if (!modified) {
+    for (let i = 0; i < 26; ++i) {
+      if (i !== idx && node.child[i]) {
+        if (dfs(searchWord, node.child[i], pos + 1, true)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
+class Trie {
+  constructor() {
+    // 是一个单词的结尾就把这个变量转为true,但是不影响继续往下遍历
+    this.isFinished = false;
+    this.child = new Array(26).fill(0);
+  }
+}
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * var obj = new MagicDictionary()
+ * obj.buildDict(dictionary)
+ * var param_2 = obj.search(searchWord)
+ */
