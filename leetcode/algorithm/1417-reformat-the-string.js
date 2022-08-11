01e70241cb4b88e6c24a1cbdb1f9ad3ded3daa25
liuhe <ok1332983774@163.com>
2022-08-11 09:34:44 +0800

    1417. 重新格式化字符串 官方题解 双指针

--- leetcode/algorithm/1417-reformat-the-string.js
+++ leetcode/algorithm/1417-reformat-the-string.js
@@ -25,11 +25,44 @@ var reformat = function (s) {
   });
   if (Math.abs(isEqual) > 1) return result;
   // isEqual: 1,0,-1三种情况,0就数字字母哪个在前面都行了数字多一个,那第一个就得是数字
-  if(isEqual){
-    
+  if (isEqual) {
   }
 };
 
 const isNumber = (str) => {
   return "0123456789".includes(str);
+};
+
+/**
+ * 双指针
+ */
+var reformat = function (s) {
+  let sumDigit = 0;
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    if (isDigit(c)) {
+      sumDigit++;
+    }
+  }
+  let sumAlpha = s.length - sumDigit;
+  if (Math.abs(sumDigit - sumAlpha) > 1) {
+    return "";
+  }
+  
+  let flag = sumDigit > sumAlpha;
+  // 这之前的逻辑差不多
+  const arr = [...s];
+  for (let i = 0, j = 1; i < s.length; i += 2) {
+    if (isDigit(arr[i]) !== flag) {
+      while (isDigit(arr[j]) !== flag) {
+        j += 2;
+      }
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  return arr.join("");
+};
+
+const isDigit = (ch) => {
+  return parseFloat(ch).toString() === "NaN" ? false : true;
 };
