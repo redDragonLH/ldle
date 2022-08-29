a52419c83ff9be7a190b09dc71d04b96e4d3f88b
LH <ok1332983774@163.com>
2022-08-29 08:25:29 +0800

    662. 二叉树最大宽度 未完成

---
+++ leetcode/algorithm/662-maximum-width-of-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * 662. 二叉树最大宽度
+ * 
+ * 给你一棵二叉树的根节点 root ，返回树的 最大宽度 。
+ * 
+ * 树的 最大宽度 是所有层中最大的 宽度 。
+ * 
+ * 每一层的 宽度 被定义为该层最左和最右的非空节点（即，两个端点）之间的长度。将这个二叉树视作与满二叉树结构相同，两端点间会出现一些延伸到这一层的 null 节点，这些 null 节点也计入长度。
+ * 
+ * 题目数据保证答案将会在  32 位 带符号整数范围内。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var widthOfBinaryTree = function(root) {
+
+};
