8ee0bf7d3c900910b9af10b3c8d9df58daf570ac
liuhe <ok1332983774@163.com>
2022-09-13 09:54:27 +0800

    670. 最大交换 官方题解

--- leetcode/algorithm/670-maximum-swap.js
+++ leetcode/algorithm/670-maximum-swap.js
@@ -40,3 +40,28 @@ var maximumSwap = function (num) {
   }
 
 };
+
+/**
+ * 官方题解
+ * @param {*} num 
+ * @returns 
+ */
+var maximumSwap = function(num) {
+    const charArray = [...'' + num];
+    const n = charArray.length;
+    let maxNum = num;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            swap(charArray, i, j);
+            maxNum = Math.max(maxNum, parseInt(charArray.join('')));
+            swap(charArray, i, j);
+        }
+    }
+    return maxNum;
+}
+
+const swap = (charArray, i, j) => {
+    const temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+};
