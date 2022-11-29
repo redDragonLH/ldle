f83e2fd99dab573fd01700d0acd17d31eb9cec8a
liuhe <ok1332983774@163.com>
2022-11-29 09:28:28 +0800

    1758. 生成交替二进制字符串的最少操作数 官方题解 归纳总结

--- leetcode/algorithm/1758-minimum-changes-to-make-alternating-binary-string.js
+++ leetcode/algorithm/1758-minimum-changes-to-make-alternating-binary-string.js
@@ -27,3 +27,24 @@ var minOperations = function (s) {
   }
   return count;
 };
+
+/**
+ * 只会有两个结果 101...,010...
+ * 
+ * 理论基础:变成这两种不同的交替二进制字符串所需要的最少操作数加起来等于 s 的长度，我们只需要计算出变为其中一种字符串的最少操作数，就可以推出另一个最少操作数，然后取最小值即可。
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function (s) {
+  let cnt = 0;
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    // 判断当前字符是否符合当前位置的逻辑 010101...
+    if (c !== String.fromCharCode("0".charCodeAt() + (i % 2))) {
+        //不等于就要改变,那么动作量+1
+      cnt++;
+    }
+  }
+  // 默认此次没有改变的下个模式一定会改变
+  return Math.min(cnt, s.length - cnt);
+};
