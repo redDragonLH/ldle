6e4a665c0df73cee45c2cde150ed5a77f822b0a4
liuhe <ok1332983774@163.com>
2022-11-30 09:26:12 +0800

    895. 最大频率栈 官方题解 注释

--- leetcode/algorithm/895-maximum-frequency-stack.js
+++ leetcode/algorithm/895-maximum-frequency-stack.js
@@ -19,16 +19,21 @@ var FreqStack = function () {
 };
 
 FreqStack.prototype.push = function (val) {
+    // 保存每个val的插入数量
   this.freq.set(val, (this.freq.get(val) || 0) + 1);
   if (!this.group.has(this.freq.get(val))) {
     this.group.set(this.freq.get(val), []);
   }
+  // 找到当前val的 数量 对应的组,把val插进去
+  // 当一个val 达到一个新的 数量,为这个 数量 新建一个桶或者说组,然后在这个组里按顺序插入推出
+  // 这个思路有意思,不过就是数量太多会不会炸
   this.group.get(this.freq.get(val)).push(val);
   this.maxFreq = Math.max(this.maxFreq, this.freq.get(val));
 };
 
 FreqStack.prototype.pop = function () {
     // map 保证了顺序吗
+    // 好像也不是
   const val = this.group.get(this.maxFreq)[
     this.group.get(this.maxFreq).length - 1
   ];
