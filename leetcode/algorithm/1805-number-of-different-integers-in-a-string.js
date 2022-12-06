a0a6fe68b7867f2cc9f039a600d5a4dcce235baf
liuhe <ok1332983774@163.com>
2022-12-06 09:23:08 +0800

    1805. 字符串中不同整数的数目 字符串比较 失败

---
+++ leetcode/algorithm/1805-number-of-different-integers-in-a-string.js
@@ -0,0 +1,42 @@
+/**
+ * 1805. 字符串中不同整数的数目
+ *
+ * 给你一个字符串 word ，该字符串由数字和小写英文字母组成。
+ *
+ * 请你用空格替换每个不是数字的字符。例如，"a123bc34d8ef34" 将会变成 " 123  34 8  34" 。注意，剩下的这些整数为（相邻彼此至少有一个空格隔开）："123"、"34"、"8" 和 "34" 。
+ *
+ * 返回对 word 完成替换后形成的 不同 整数的数目。
+ *
+ * 只有当两个整数的 不含前导零 的十进制表示不同， 才认为这两个整数也不同。
+ */
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numDifferentIntegers = function (word) {
+  let numberStr = word.replaceAll(/\D/gi, " ");
+  let set = new Set();
+  let numberArr = numberStr.split(" ");
+  for (const item of numberArr) {
+    let num = "";
+    let head = true;
+    if (item.length == 1) {
+      set.add(item);
+      continue;
+    }
+    for (let i = 0; i < item.length; i++) {
+      if (item[i] !== "0") head = false;
+      if (head) {
+        continue;
+      } else {
+        num += item[i];
+      }
+    }
+    num && set.add(num);
+  }
+  return set.size;
+};
+/**
+ * 字符串判断,要判断前导零以及整个数字都是0 逻辑过于复杂,不如直接转数字
+ */
