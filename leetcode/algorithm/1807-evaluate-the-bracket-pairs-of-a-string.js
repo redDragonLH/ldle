ba10cd77427d561cf41fdcba279a89dc9fa99a8b
liuhe <ok1332983774@163.com>
2023-01-12 09:50:16 +0800

    1807. 替换字符串中的括号内容 哈希表

--- leetcode/algorithm/1807-evaluate-the-bracket-pairs-of-a-string.js
+++ leetcode/algorithm/1807-evaluate-the-bracket-pairs-of-a-string.js
@@ -25,3 +25,37 @@
     // 无法处理 (fy)tzv(ege)r 在不存在key时的替换
     return s.replaceAll(/\(.*\)/g,'?'); 
 };
+
+/**
+ * 哈希表,
+ */
+ var evaluate = function(s, knowledge) {
+    const dict = new Map();
+    for (const kd of knowledge) {
+        dict.set(kd[0], kd[1]);
+    }
+    let addKey = false;
+    let key = '';
+    let res = '';
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (c === '(') {
+            addKey = true;
+        } else if (c === ')') {
+            if (dict.has(key)) {
+                res += dict.get(key);
+            } else {
+                res += '?';
+            }
+            addKey = false;
+            key = '';
+        } else {
+            if (addKey) {
+                key += c;
+            } else {
+                res += c;
+            }
+        }
+    }
+    return res;
+};
