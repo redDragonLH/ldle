09fb1d39c3bbd2f5ee1eb6b9f68f1d92ae2080f2
liuhe <ok1332983774@163.com>
2023-04-03 14:57:01 +0800

    1053. 交换一次的先前排列 贪心

--- leetcode/algorithm/1053-previous-permutation-with-one-swap.js
+++ leetcode/algorithm/1053-previous-permutation-with-one-swap.js
@@ -12,29 +12,54 @@
  * @return {number[]}
  */
 var prevPermOpt1 = function (arr) {
-    // 但是这逻辑不能保证最小,因为位最小才行
-    // 第2,3位交换肯定比 第1,4位交换的结果小,
-    // 双轮询没有达到这个效果
+  // 但是这逻辑不能保证最小,因为位最小才行
+  // 第2,3位交换肯定比 第1,4位交换的结果小,
+  // 双轮询没有达到这个效果
 
-    // 可以加缓存
-    let len = arr.length
-    let jIndex = 0;
-    let iIndex = 0;
-    for (let i = len-1; i >= 0; i--) {
-        for (let j = i-1; j >=0; j--) {
-            if(arr[j]>arr[i]){
-                if(jIndex<j){
-                    jIndex = j;
-                    iIndex = i
-                }
-            }
+  // 可以加缓存
+  let len = arr.length;
+  let jIndex = 0;
+  let iIndex = 0;
+  for (let i = len - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] > arr[i]) {
+        if (jIndex < j) {
+          jIndex = j;
+          iIndex = i;
         }
+      }
     }
-    if(iIndex !== 0&& jIndex!==0){
-        [arr[i],arr[j]] = [arr[j],arr[i]]
-    }
-    return arr
+  }
+  if (iIndex !== 0 && jIndex !== 0) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
 };
 /**
  * 解决不了逻辑边界问题
  */
+
+/**
+ * 双循环确实可以解决这个问题
+ * @param {} arr 
+ * @returns 
+ */
+var prevPermOpt1 = function (arr) {
+  const n = arr.length;
+  for (let i = n - 2; i >= 0; i--) {
+    // 第一层遍历就检查临近数据
+    // 只要有非递减数据就肯定有
+    if (arr[i] > arr[i + 1]) {
+        // 找到位置之后从尾部再次遍历找到最小的位,也就是最右侧的可以用和i互换的元素
+      let j = n - 1;
+      while (arr[j] >= arr[i] || arr[j] == arr[j - 1]) {
+        j--;
+      }
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      break;
+    }
+  }
+  return arr;
+};
