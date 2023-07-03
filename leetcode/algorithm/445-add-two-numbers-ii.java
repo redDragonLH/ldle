c6ed92777e502e32516a31605381def68e52d79c
liuhe <ok1332983774@163.com>
2023-07-03 09:18:47 +0800

    两数之和 ii 官方题解

---
+++ leetcode/algorithm/445-add-two-numbers-ii.java
@@ -0,0 +1,56 @@
+/**
+ * 两数之和 ii
+ * 
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * 进阶：
+ * 如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+ * 
+ * 注意： 不能把所有数字拿出来拼接到一起然后转数字相加再转字符串，因为 肯定会遇到bigint也无法解决的大数
+ */
+
+ /**
+  * 官方题解代码： 思路一样，代码精简
+  */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+  * java
+  * */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //Deque 双端队列,两边都可进出
+        Deque<Integer> stack1 = new ArrayDeque<Integer>();
+        Deque<Integer> stack2 = new ArrayDeque<Integer>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        int carry = 0;
+        ListNode ans = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int a = stack1.isEmpty() ? 0 : stack1.pop();
+            int b = stack2.isEmpty() ? 0 : stack2.pop();
+            int cur = a + b + carry;
+            carry = cur / 10;
+            cur %= 10;
+            ListNode curnode = new ListNode(cur);
+            curnode.next = ans;
+            ans = curnode;
+        }
+        return ans;
+    }
+}
+  class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
