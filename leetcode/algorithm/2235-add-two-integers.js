84b9ae67e9ce030365255bd0b5ea1777153fe605
liuhe <ok1332983774@163.com>
2023-08-19 09:06:10 +0800

    2235. 两整数相加 模拟

---
+++ leetcode/algorithm/2235-add-two-integers.js
@@ -0,0 +1,19 @@
+/**
+ * 2235. 两整数相加
+ * 
+ * 给你两个整数 num1 和 num2，返回这两个整数的和。
+ */
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    return num1+num2
+};
+
+/**
+ * 执行用时：60 ms, 在所有 JavaScript 提交中击败了71.15%的用户
+ * 内存消耗：40.04 MB, 在所有 JavaScript 提交中击败了62.09%的用户
+ */
