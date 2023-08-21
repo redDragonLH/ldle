34d2ae17e8230160467696445f4e3fd541bc5902
liuhe <ok1332983774@163.com>
2023-08-21 09:33:51 +0800

    2337. 移动片段得到字符串 比较数量。失败

---
+++ leetcode/algorithm/2337-move-pieces-to-obtain-a-string.js
@@ -0,0 +1,40 @@
+/**
+ * 2337. 移动片段得到字符串
+ *
+ * 给你两个字符串 start 和 target ，长度均为 n 。每个字符串 仅 由字符 'L'、'R' 和 '_' 组成，其中：
+ *
+ * 字符 'L' 和 'R' 表示片段，其中片段 'L' 只有在其左侧直接存在一个 空位 时才能向 左 移动，而片段 'R' 只有在其右侧直接存在一个 空位 时才能向 右 移动。
+ * 字符 '_' 表示可以被 任意 'L' 或 'R' 片段占据的空位。
+ * 如果在移动字符串 start 中的片段任意次之后可以得到字符串 target ，返回 true ；否则，返回 false 。
+ */
+
+/**
+ * 去掉 _ 后LR组成的字符串相等,_ 数量一样
+ * @param {string} start
+ * @param {string} target
+ * @return {boolean}
+ */
+var canChange = function (start, target) {
+    let del_start = start.split("_").join("")
+    let del_target = target.split("_").join("")
+    console.log(del_start,del_target)
+    if(del_start!==del_target) return false;
+
+    let start_num = 0
+    let target_num = 0
+    for (let i = 0; i < start.length; i++) {
+        if(start[i] === '_'){
+            start_num++
+        }
+        if(target[i] === '_'){
+            target_num++
+        }
+    }
+    console.log(start_num,target_num)
+
+    if(start_num === target_num) return true;
+    return false;
+};
+/**
+ * 失败,还是要考虑左右移动的问题
+ */
