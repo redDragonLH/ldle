d464b641075ac284b9911efc8be143aac85020a6
liuhe <ok1332983774@163.com>
2023-10-10 10:47:09 +0800

    2731. 移动机器人 官方题解

--- leetcode/algorithm/2731-movement-of-robots.js
+++ leetcode/algorithm/2731-movement-of-robots.js
@@ -46,3 +46,21 @@ var sumDistance = function (nums, s, d) {
 /**
  * 超出时间限制
  */
+
+var sumDistance = function(nums, s, d) {
+    const mod = 1e9 + 7;
+    n = nums.length;
+    pos = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        pos[i] = s[i] === 'L' ? nums[i] - d : nums[i] + d;
+    }
+    pos.sort((a, b) => a - b);
+    let res = 0;
+    // 单层循环就够吗?
+    // 有向图之中有累加规则
+    for (let i = 1; i < n; i++) {
+        res += (pos[i] - pos[i - 1]) * i % mod * (n - i) % mod;
+        res %= mod;
+    }
+    return res;
+};
