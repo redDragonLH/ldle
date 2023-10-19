991dbc999c03fafeef79e46cb41bfcc6dabeace9
liuhe <ok1332983774@163.com>
2023-10-19 09:31:44 +0800

    1726. 同积元组 映射

--- leetcode/algorithm/1726-tuple-with-same-product.js
+++ leetcode/algorithm/1726-tuple-with-same-product.js
@@ -6,27 +6,43 @@
 /**
  * 前缀和?但是要怎么保证元素不重复,毕竟顺序是乱的
  * 如果有一组是满足条件的,那么按照交换律,就能生成四组满足条件的
- * 超级暴力的方案,果然超出时间限制
+ * 
  * @param {number[]} nums
  * @return {number}
  */
 var tupleSameProduct = function (nums) {
   let reslult = 0;
   let len = nums.length;
+  let mapping = new Map();
   for (let i = 0; i < len; i++) {
     for (let j = i + 1; j < len; j++) {
-      for (let k = j + 1; k < len; k++) {
-        for (let l = k + 1; l < len; l++) {
-          if (nums[i] * nums[j] === nums[k] * nums[l]) {
-            reslult += 8;
-          } else if (nums[i] * nums[k] === nums[j] * nums[l]) {
-            reslult += 8;
-          } else if (nums[i] * nums[l] === nums[j] * nums[k]) {
-            reslult += 8;
-          }
-        }
+      if (i != j) {
+        let current = mapping.get(nums[i] * nums[j]) || [];
+        mapping.set(nums[i] * nums[j], [...current, [i, j]]);
       }
     }
   }
+  mapping.forEach((value, k) => {
+    if (value.length > 1) {
+      reslult += parseInt(
+        (factorial(value.length) / (2 * factorial(value.length - 2))) * 8
+      );
+    }
+  });
   return reslult;
 };
+function factorial(n) {
+  if (0 === n) {
+    return 1;
+  }
+  let res = 1;
+  for (let i = 1; i <= n; ++i) {
+    res *= i;
+  }
+  return res;
+}
+/**
+ * 前缀和maping
+ * 执行用时：2136 ms, 在所有 JavaScript 提交中击败了7.14%的用户
+ * 内存消耗：204.25 MB, 在所有 JavaScript 提交中击败了7.14%的用户
+ */
