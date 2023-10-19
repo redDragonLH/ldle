bd5f2502120e5925e47a2105e964270d2ed3eec6
liuhe <ok1332983774@163.com>
2023-10-19 09:46:51 +0800

    1726. 同积元组 映射，拆分逻辑计算

--- leetcode/algorithm/1726-tuple-with-same-product.js
+++ leetcode/algorithm/1726-tuple-with-same-product.js
@@ -24,6 +24,7 @@ var tupleSameProduct = function (nums) {
       }
     }
   }
+  // 统一计算消耗大.逻辑复杂
   mapping.forEach((value, k) => {
     if (reslultMapp.has(value.length)) {
       reslult += reslultMapp.get(value.length);
@@ -61,3 +62,32 @@ function factorial(n) {
  * 执行用时：1880 ms, 在所有 JavaScript 提交中击败了7.14%的用户
  * 内存消耗：204.75 MB, 在所有 JavaScript 提交中击败了7.14%的用户
  */
+
+/**
+ * 最快题解
+ * @param {number[]} nums
+ * @return {number}
+ */
+var tupleSameProduct = function (nums) {
+  const hash = new Map();
+  let ans = 0;
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    // 这里保证位置不会出现重复
+    for (let j = i + 1; j < n; j++) {
+      const product = nums[i] * nums[j];
+      if (hash.has(product)) {
+        // 核心在这里
+        // 哦,这是没步计算都不同的,他拆掉了统一的计算,而是分开了
+        // 比如 
+        // 第一次遇到这个结果,has就是fals
+        // 2. true => 8* 1 =>reuslt+=
+        // 3. true =>8*2 =>reuslt+=
+        // 每多一个就重新计算一遍,因为其中一边是新出现的对,所以不可能会出现计算重复
+        ans += 8 * hash.get(product);
+      }
+      hash.set(product, (hash.get(product) || 0) + 1);
+    }
+  }
+  return ans;
+};
