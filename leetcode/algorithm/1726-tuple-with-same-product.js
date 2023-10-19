89be191fbbb54ce9f0d6d7b8df8976b671c4c76f
liuhe <ok1332983774@163.com>
2023-10-19 09:38:45 +0800

    1726. 同积元组 增加缓存

--- leetcode/algorithm/1726-tuple-with-same-product.js
+++ leetcode/algorithm/1726-tuple-with-same-product.js
@@ -6,10 +6,12 @@
 /**
  * 前缀和?但是要怎么保证元素不重复,毕竟顺序是乱的
  * 如果有一组是满足条件的,那么按照交换律,就能生成四组满足条件的
- * 
+ *
  * @param {number[]} nums
  * @return {number}
  */
+let reslultMapp = new Map();
+
 var tupleSameProduct = function (nums) {
   let reslult = 0;
   let len = nums.length;
@@ -23,15 +25,24 @@ var tupleSameProduct = function (nums) {
     }
   }
   mapping.forEach((value, k) => {
+    if (reslultMapp.has(value.length)) {
+      reslult += reslultMapp.get(value.length);
+      return true;
+    }
+
     if (value.length > 1) {
-      reslult += parseInt(
+      let count = parseInt(
         (factorial(value.length) / (2 * factorial(value.length - 2))) * 8
       );
+      reslult += count;
+      reslultMapp.set(value.length, count);
     }
   });
   return reslult;
 };
+let fMapping = new Map();
 function factorial(n) {
+  if (fMapping.has(n)) return fMapping.get(n);
   if (0 === n) {
     return 1;
   }
@@ -39,10 +50,14 @@ function factorial(n) {
   for (let i = 1; i <= n; ++i) {
     res *= i;
   }
+  fMapping.set(n, res);
   return res;
 }
 /**
  * 前缀和maping
- * 执行用时：2136 ms, 在所有 JavaScript 提交中击败了7.14%的用户
- * 内存消耗：204.25 MB, 在所有 JavaScript 提交中击败了7.14%的用户
+ * 给阶乘方法添加缓存并没有减少时间
+ * 增加结果缓存也没有减少时间
+ * 那耗时都在哪里?
+ * 执行用时：1880 ms, 在所有 JavaScript 提交中击败了7.14%的用户
+ * 内存消耗：204.75 MB, 在所有 JavaScript 提交中击败了7.14%的用户
  */
