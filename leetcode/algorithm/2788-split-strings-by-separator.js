f5ea4d98b9d0fd995fd6e550ac0d6ba9f1f6c9fc
he.liu <ok1332983774@163.com>
2024-01-20 01:00:10 +0800

    2788. 按分隔符拆分字符串 遍历

---
+++ leetcode/algorithm/2788-split-strings-by-separator.js
@@ -0,0 +1,27 @@
+/**
+ * 2788. 按分隔符拆分字符串
+ *
+ * 给你一个字符串数组 words 和一个字符 separator ，请你按 separator 拆分 words 中的每个字符串。
+ * 返回一个由拆分后的新字符串组成的字符串数组，不包括空字符串 。
+ * 注意
+ *  * separator 用于决定拆分发生的位置，但它不包含在结果字符串中。
+ *  * 拆分可能形成两个以上的字符串。
+ *  * 结果字符串必须保持初始相同的先后顺序。
+ */
+
+/**
+ * @param {string[]} words
+ * @param {character} separator
+ * @return {string[]}
+ */
+var splitWordsBySeparator = function (words, separator) {
+  let result = [];
+  for (const word of words) {
+    result.push(...word.split(separator).filter((v, i) => v !== ""));
+  }
+  return result;
+};
+/**
+ * 执行用时：119 ms, 在所有 JavaScript 提交中击败了14.00%的用户
+ * 内存消耗：58.29 MB, 在所有 JavaScript 提交中击败了6.00%的用户
+ */
