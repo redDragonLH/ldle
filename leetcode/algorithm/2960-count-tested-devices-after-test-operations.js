79c5d80ab4e0dc03f0097da873d5a88c807a77ba
liuhe <ok1332983774@163.com>
2024-05-10 09:04:50 +0800

    2960. 统计已测试设备 遍历 累减

--- leetcode/algorithm/2960-count-tested-devices-after-test-operations.js
+++ leetcode/algorithm/2960-count-tested-devices-after-test-operations.js
@@ -1,6 +1,6 @@
 /**
  * 2960. 统计已测试设备
- * 
+ *
  * 给你一个长度为 n 、下标从 0 开始的整数数组 batteryPercentages ，表示 n 个设备的电池百分比。
  * 你的任务是按照顺序测试每个设备 i，执行以下测试操作：
  *  * 如果 batteryPercentages[i] 大于 0：
@@ -16,14 +16,18 @@
  * @param {number[]} batteryPercentages
  * @return {number}
  */
-var countTestedDevices = function(batteryPercentages) {
-    let result = 0;
-    let accumulate = 0;
-    for (const battery of batteryPercentages) {
-        if(battery -accumulate >0){
-            result++
-            accumulate++
-        }
+var countTestedDevices = function (batteryPercentages) {
+  let result = 0;
+  let accumulate = 0;
+  for (const battery of batteryPercentages) {
+    if (battery - accumulate > 0) {
+      result++;
+      accumulate++;
     }
-    return result
+  }
+  return result;
 };
+/**
+ * 执行用时：60 ms, 在所有 JavaScript 提交中击败了80.49%的用户
+ * 内存消耗：50.42 MB, 在所有 JavaScript 提交中击败了26.83%的用户
+ */
