7aec10a6a2af80acb45d3454dc66238ea0af504d
liuhe <ok1332983774@163.com>
2024-06-14 09:15:56 +0800

    2786. 访问数组中的位置使分数最大 失败

---
+++ leetcode/algorithm/2786-visit-array-positions-to-maximize-score.js
@@ -0,0 +1,41 @@
+/**
+ * 2786. 访问数组中的位置使分数最大
+ *
+ * 给你一个下标从 0 开始的整数数组 nums 和一个正整数 x 。
+ * 你 一开始 在数组的位置 0 处，你可以按照下述规则访问数组中的其他位置：
+ *  * 如果你当前在位置 i ，那么你可以移动到满足 i < j 的 任意 位置 j 。
+ *  * 对于你访问的位置 i ，你可以获得分数 nums[i] 。
+ *  * 如果你从位置 i 移动到位置 j 且 nums[i] 和 nums[j] 的 奇偶性 不同，那么你将失去分数 x 。
+ * 请你返回你能得到的 最大 得分之和。
+ * 注意 ，你一开始的分数为 nums[0] 。
+ */
+/**
+ * 遍历奇偶性,是否与0位元素一样
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+var maxScore = function (nums, x) {
+  let zeroStatus = nums[0] % 2 === 0;
+  let odd = 0,
+    even = 0;
+  if (zeroStatus) {
+    even = nums[0];
+    odd = -nums[0];
+  } else {
+    odd = nums[0];
+    even -= nums[0];
+  }
+
+  for (const num of nums) {
+    if (num % 2 === 0) {
+      even += num;
+    } else {
+      odd += num;
+    }
+  }
+  return Math.max(even, odd);
+};
+/**
+ * 失败,应该有条件我没理解
+ */
