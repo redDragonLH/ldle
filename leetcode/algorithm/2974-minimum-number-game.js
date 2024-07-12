6ab87d42312b152a8967fa4e22c06636f73fab3c
liuhe <ok1332983774@163.com>
2024-07-12 09:10:23 +0800

    2974. 最小数字游戏 遍历 优化 失败

--- leetcode/algorithm/2974-minimum-number-game.js
+++ leetcode/algorithm/2974-minimum-number-game.js
@@ -1,6 +1,6 @@
 /**
  * 2974. 最小数字游戏
- * 
+ *
  * 你有一个下标从 0 开始、长度为 偶数 的整数数组 nums ，同时还有一个空数组 arr 。Alice 和 Bob 决定玩一个游戏，游戏中每一轮 Alice 和 Bob 都会各自执行一次操作。游戏规则如下：
  *  * 每一轮，Alice 先从 nums 中移除一个 最小 元素，然后 Bob 执行同样的操作。
  *  * 接着，Bob 会将移除的元素添加到数组 arr 中，然后 Alice 也执行同样的操作。
@@ -14,12 +14,12 @@
  */
 var numberGame = function (nums) {
   nums.sort((a, b) => a - b);
-  let res = [];
   for (let i = 0; i < nums.length; i += 2) {
-    res.push(nums[i + 1]);
-    res.push(nums[i]);
+    let temp = nums[i];
+    nums[i] = nums[i + 1];
+    nums[i + 1] = temp;
   }
-  return res;
+  return nums;
 };
 /**
  * 执行用时：79 ms, 在所有 JavaScript 提交中击败了47.96%的用户
