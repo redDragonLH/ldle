3dd1d57de5c0e22a6be7c0040646152da7df424a
liuhe <ok1332983774@163.com>
2024-07-13 09:16:51 +0800

    3011. 判断一个数组是否可以变为有序 官方题解

--- leetcode/algorithm/3011-find-if-array-can-be-sorted.js
+++ leetcode/algorithm/3011-find-if-array-can-be-sorted.js
@@ -26,10 +26,44 @@ var canSortArray = function (nums) {
   });
   // 那就是还得找出区间中最大最小的数
   for (let i = 1; i < binaryArr.length; i++) {
-    if (binaryArr[i] !== binaryArr[i - 1] && nums[i]<nums[i-1]) {
+    if (binaryArr[i] !== binaryArr[i - 1] && nums[i] < nums[i - 1]) {
       return false;
     }
-    
   }
   return true;
+};
+
+/**
+ * 官方题解
+ */
+var canSortArray = function (nums) {
+  let lastCnt = 0;
+  let lastGroupMax = 0;
+  let curGroupMax = 0;
+
+  for (let num of nums) {
+    let curCnt = countBits(num);
+    if (curCnt === lastCnt) {
+      curGroupMax = Math.max(curGroupMax, num);
+    } else {
+    // 换区间
+      lastCnt = curCnt;
+      lastGroupMax = curGroupMax;
+      curGroupMax = num;
+    }
+    // 这个思路确实没问题,只不过我的思路太啰嗦了
+    if (num < lastGroupMax) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const countBits = (num) => {
+  let count = 0;
+  while (num !== 0) {
+    count++;
+    num &= num - 1;
+  }
+  return count;
 };
