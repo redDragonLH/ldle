fcddf33e44469e9954e9e24251661e248ad2b9e7
he.liu <ok1332983774@163.com>
2024-08-04 00:08:24 +0800

    572. 另一棵树的子树 第三方题解 字符串匹配

---
+++ leetcode/algorithm/572-subtree-of-another-tree.js
@@ -0,0 +1,24 @@
+/**
+ * 572. 另一棵树的子树
+ * 
+ * 给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
+ * 二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = (s, t) => (JSON.stringify(s).indexOf(JSON.stringify(t)))>-1?true:false
+/**
+ * 第三方题解， 好邪道的答案~~
+ */
