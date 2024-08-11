917e670c645349e849dc305b8d28a6afc336de09
LH <ok1332983774@163.com>
2024-08-11 15:20:54 +0800

    1035. 不相交的线 官方题解

---
+++ leetcode/algorithm/1035-uncrossed-lines.js
@@ -0,0 +1,34 @@
+/**
+ * 1035. 不相交的线
+ * 
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足：
+ *  *  nums1[i] == nums2[j]
+ *  * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ */
+/**
+ * 动态规划
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxUncrossedLines = function(nums1, nums2) {
+    const m = nums1.length, n = nums2.length;
+    // 二维数据结构
+    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+    for (let i = 1; i <= m; i++) {
+        const num1 = nums1[i - 1];
+        
+        for (let j = 1; j <= n; j++) {
+            const num2 = nums2[j - 1];
+            if (num1 === num2) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+};
