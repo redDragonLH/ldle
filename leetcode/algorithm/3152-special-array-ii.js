5ae2cf13c88cb6fd3b9b00b29e89ff42d9d2a14c
liuhe <ok1332983774@163.com>
2024-08-14 09:39:19 +0800

    3152. 特殊数组 II 官方题解 动态规划

--- leetcode/algorithm/3152-special-array-ii.js
+++ leetcode/algorithm/3152-special-array-ii.js
@@ -7,6 +7,7 @@
  */
 
 /**
+ * 这怎么优化嘞
  * @param {number[]} nums
  * @param {number[][]} queries
  * @return {boolean[]}
@@ -29,3 +30,30 @@ var isArraySpecial = function (nums, queries) {
 /**
  * 超时
  */
+
+/**
+ * 官方题解 动态规划
+ * @param {*} nums
+ * @param {*} queries
+ * @returns
+ */
+var isArraySpecial = function (nums, queries) {
+  const n = nums.length;
+  // 数据结构.
+  const dp = new Array(n).fill(1);
+  for (let i = 1; i < n; i++) {
+    // ^ 二进制操作符,异或, 两个操作数有且仅有一个对应的二进制位为 1 时，该位的结果值为 1
+    // & 二进制操作符,与, 两个操作数的对应位都为 1 时，该位的结果值为 1
+    // 对于给定的元素 a,b，当满足 (a^b)&1=1 时，则 a,b 的奇偶性不同，否则奇偶性相同；
+    if ((nums[i] ^ nums[i - 1]) & 1) {
+      // 如果连续相同那么后一个元素就比前一个元素多一.
+      dp[i] = dp[i - 1] + 1;
+    }
+  }
+
+  const res = [];
+  for (const [x, y] of queries) {
+    res.push(dp[y] >= y - x + 1);
+  }
+  return res;
+};
