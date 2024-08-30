9a6da111d564ee8cab0faedbce616436f7ec85a0
liuhe <ok1332983774@163.com>
2024-08-30 08:54:38 +0800

    3153. 所有数对中数位不同之和 超时

---
+++ leetcode/algorithm/3153-sum-of-digit-differences-of-all-pairs.js
@@ -0,0 +1,34 @@
+/**
+ * 3153. 所有数对中数位不同之和
+ *
+ * 你有一个数组 nums ，它只包含 正 整数，所有正整数的数位长度都 相同 。
+ * 两个整数的 数位不同 指的是两个整数 相同 位置上不同数字的数目。
+ * 请你返回 nums 中 所有 整数对里，数位不同之和。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumDigitDifferences = function (nums) {
+  let n = nums.length;
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      console.log(i, j, getDiff(nums[i], nums[j]));
+      ans += getDiff(nums[i], nums[j]);
+    }
+  }
+  return ans;
+};
+let getDiff = (a, b) => {
+  let ans = 0;
+  while (a || b) {
+    ans += a % 10 === b % 10 ? 0 : 1;
+    a = Math.floor(a / 10);
+    b = Math.floor(b / 10);
+  }
+  return ans;
+};
+/**
+ * 超时
+ */
