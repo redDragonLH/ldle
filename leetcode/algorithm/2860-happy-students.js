4fcda789c41ab1506cb1eedeef9fc91987de8281
liuhe <ok1332983774@163.com>
2024-09-04 09:01:55 +0800

    2860. 让所有学生保持开心的分组方法数 遍历

---
+++ leetcode/algorithm/2860-happy-students.js
@@ -0,0 +1,31 @@
+/**
+ * 2860. 让所有学生保持开心的分组方法数
+ *
+ * 给你一个下标从 0 开始、长度为 n 的整数数组 nums ，其中 n 是班级中学生的总数。班主任希望能够在让所有学生保持开心的情况下选出一组学生：
+ * 如果能够满足下述两个条件之一，则认为第 i 位学生将会保持开心：
+ *  * 这位学生被选中，并且被选中的学生人数 严格大于 nums[i] 。
+ *  * 这位学生没有被选中，并且被选中的学生人数 严格小于 nums[i] 。
+ * 返回能够满足让所有学生保持开心的分组方法的数目。
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countWays = function (nums) {
+  const n = nums.length;
+  let res = 0;
+  nums.sort((a, b) => a - b);
+  for (let k = 0; k <= n; k++) {
+    // 前 k 个元素的最大值是否小于 k
+    if (k > 0 && nums[k - 1] >= k) {
+      continue;
+    }
+    // 后 n - k 个元素的最小值是否大于 k
+    if (k < n && nums[k] <= k) {
+      continue;
+    }
+    res++;
+  }
+  return res;
+};
