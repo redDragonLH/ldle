94a223cca9825cb5a953ac5836c9b56346a1c89d
liuhe <ok1332983774@163.com>
2024-09-20 09:15:49 +0800

    2376. 统计特殊整数 官方题解 DP

--- leetcode/algorithm/2376-count-special-integers.js
+++ leetcode/algorithm/2376-count-special-integers.js
@@ -23,3 +23,50 @@ var countSpecialNumbers = function (n) {
  * 超时
  * 果然超时,这个最简单的方法不行,应该可以优化,以空间换时间?
  */
+
+/**
+ * DP 的路子~~
+ * @param {number} n
+ * @return {number}
+ */
+var countSpecialNumbers = function (n) {
+  const nStr = n.toString();
+  const memo = new Map();
+
+  const dp = (mask, prefixSmaller, nStr) => {
+    if (countOnes(mask) === nStr.length) {
+      return 1;
+    }
+    const key = mask * 2 + (prefixSmaller ? 1 : 0);
+    if (!memo.has(key)) {
+      let res = 0;
+      let lowerBound = mask === 0 ? 1 : 0;
+      let upperBound = prefixSmaller ? 9 : nStr[countOnes(mask)] - "0";
+      for (let i = lowerBound; i <= upperBound; i++) {
+        if (((mask >> i) & 1) === 0) {
+          res += dp(mask | (1 << i), prefixSmaller || i < upperBound, nStr);
+        }
+      }
+      memo.set(key, res);
+    }
+    return memo.get(key);
+  };
+
+  let res = 0;
+  let prod = 9;
+  for (let i = 0; i < nStr.length - 1; i++) {
+    res += prod;
+    prod *= 9 - i;
+  }
+  res += dp(0, false, nStr);
+  return res;
+};
+
+const countOnes = (x) => {
+  let count = 0;
+  while (x) {
+    count++;
+    x &= x - 1;
+  }
+  return count;
+};
