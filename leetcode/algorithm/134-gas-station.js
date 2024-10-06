8751191bad8aab30fa6bf433d26a768258e8ae47
LH <ok1332983774@163.com>
2024-10-06 14:12:49 +0800

    134. 加油站 遍历

--- leetcode/algorithm/134-gas-station.js
+++ leetcode/algorithm/134-gas-station.js
@@ -1,26 +1,43 @@
 /**
  * 134. 加油站
- * 
+ *
  * 在一条环路上有 N 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
  * 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
  * 如果你可以绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1。
- * 说明: 
+ * 说明:
  * 如果题目有解，该答案即为唯一答案。
  * 输入数组均为非空数组，且长度相同。
  * 输入数组中的元素均为非负数。
  */
 
 /**
+ * 循环整个gas
+ * 从每个index重新计算是否能走到下一个位置
  * @param {number[]} gas
  * @param {number[]} cost
  * @return {number}
  */
-var canCompleteCircuit = function(gas, cost) {
-    const len = gas.length;
-    let stsrt=[]
-    for (let i = 0; i < len; i++) {
-        if(gas[i]-cost[i]) stsrt.push(i)
+var canCompleteCircuit = function (gas, cost) {
+  const n = gas.length;
+  let i = 0;
+  while (i < n) {
+    let sumOfGas = 0,
+      sumOfCost = 0;
+    let cnt = 0;
+    while (cnt < n) {
+      const j = (i + cnt) % n;
+      sumOfGas += gas[j];
+      sumOfCost += cost[j];
+      if (sumOfCost > sumOfGas) {
+        break;
+      }
+      cnt++;
     }
-    console.log(stsrt);
+    if (cnt === n) {
+      return i;
+    } else {
+      i = i + cnt + 1;
+    }
+  }
+  return -1;
 };
-canCompleteCircuit([1,2,3,4,5],[3,4,5,1,2])
