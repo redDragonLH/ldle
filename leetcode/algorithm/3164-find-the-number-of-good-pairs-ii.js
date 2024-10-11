fe6c0541eb8c006c0da62b23ee066d8bfa3136fa
liuhe <ok1332983774@163.com>
2024-10-11 09:10:08 +0800

    3164. 优质数对的总数 II 官方题解

--- leetcode/algorithm/3164-find-the-number-of-good-pairs-ii.js
+++ leetcode/algorithm/3164-find-the-number-of-good-pairs-ii.js
@@ -25,3 +25,31 @@ var numberOfPairs = function (nums1, nums2, k) {
   }
   return count;
 };
+
+/**
+ * 官方题解
+ */
+var numberOfPairs = function (nums1, nums2, k) {
+  const count = {};
+  const count2 = {};
+  let res = 0,
+    max1 = 0;
+  // 压缩,合并同类项
+  for (let num of nums1) {
+    count[num] = (count[num] || 0) + 1;
+    max1 = Math.max(max1, num);
+  }
+  for (let num of nums2) {
+    count2[num] = (count2[num] || 0) + 1;
+  }
+  for (let a in count2) {
+    let cnt = count2[a];
+    // 缩小b 的范围,肯定不会超过max1,不能小于a*k
+    for (let b = a * k; b <= max1; b += a * k) {
+      if (b in count) {
+        res += count[b] * cnt;
+      }
+    }
+  }
+  return res;
+};
