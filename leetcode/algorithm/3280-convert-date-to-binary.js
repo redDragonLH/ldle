e7c29c0ec7c963d73f8f9cb45070aa5bb9ab7b8b
LH <ok1332983774@163.com>
2025-01-01 09:23:54 +0800

    3280. 将日期转换为二进制表示

---
+++ leetcode/algorithm/3280-convert-date-to-binary.js
@@ -0,0 +1,17 @@
+/**
+ * 3280. 将日期转换为二进制表示
+ *
+ * 给你一个字符串 date，它的格式为 yyyy-mm-dd，表示一个公历日期。
+ * date 可以重写为二进制表示，只需要将年、月、日分别转换为对应的二进制表示（不带前导零）并遵循 year-month-day 的格式。
+ * 返回 date 的 二进制 表示。
+ */
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var convertDateToBinary = function (date) {
+  return date
+    .split("-")
+    .map((s) => parseInt(s, 10).toString(2))
+    .join("-");
+};
