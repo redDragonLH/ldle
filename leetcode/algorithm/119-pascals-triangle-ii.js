10e4cd2be9e1904771ffed83689e7fa1e0dc02bb
liuhe <ok1332983774@163.com>
2025-01-28 13:56:59 +0800

    119. 杨辉三角 II 官方题解

---
+++ leetcode/algorithm/119-pascals-triangle-ii.js
@@ -0,0 +1,15 @@
+/**
+ * 119. 杨辉三角 II
+ *
+ * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ */
+
+var getRow = function (rowIndex) {
+  const row = new Array(rowIndex + 1).fill(0);
+  row[0] = 1;
+  for (let i = 1; i <= rowIndex; ++i) {
+    row[i] = (row[i - 1] * (rowIndex - i + 1)) / i;
+  }
+  return row;
+};
