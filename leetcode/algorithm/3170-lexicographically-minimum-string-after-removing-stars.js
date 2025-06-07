0efe5b80c42a2b311632786ff748a0cf9d8ca8b0
LH <ok1332983774@163.com>
2025-06-07 10:04:25 +0800

    3170. 删除星号以后字典序最小的字符串 超时

---
+++ leetcode/algorithm/3170-lexicographically-minimum-string-after-removing-stars.js
@@ -0,0 +1,37 @@
+/**
+ * 3170. 删除星号以后字典序最小的字符串
+ *
+ * 给你一个字符串 s 。它可能包含任意数量的 '*' 字符。你的任务是删除所有的 '*' 字符。
+ * 当字符串还存在至少一个 '*' 字符时，你可以执行以下操作：
+ *  * 删除最左边的 '*' 字符，同时删除该星号字符左边一个字典序 最小 的字符。如果有多个字典序最小的字符，你可以删除它们中的任意一个。
+ * 请你返回删除所有 '*' 字符以后，剩余字符连接而成的 字典序最小 的字符串。
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var clearStars = function (s) {
+  let arr = s.split("");
+  while (arr.includes("*")) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "*") {
+        // 找到左侧最近的字典序最小字符
+        let minIdx = -1;
+        let minChar = "{"; // 比 'z' 大的字符
+        for (let j = i - 1; j >= 0; j--) {
+          if (arr[j] !== "" && arr[j] < minChar) {
+            minChar = arr[j];
+            minIdx = j;
+          }
+        }
+        if (minIdx !== -1) arr[minIdx] = "";
+        arr[i] = "";
+        break; // 只处理最左边的一个星号
+      }
+    }
+  }
+  return arr.filter((c) => c !== "" && c !== "*").join("");
+};
+/**
+ * 有超时用例，可以用预处理的方式优化，前缀和？
+ */
