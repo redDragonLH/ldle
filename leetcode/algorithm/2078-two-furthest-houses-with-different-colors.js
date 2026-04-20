f1151493696dacd3aee5fbf5994def1865410b5f
liuhe <liuhe@liuhedeMacBook-Pro.local>
2026-04-20 09:43:45 +0800

    2078. 两栋颜色不同且距离最远的房子 双指针

--- leetcode/algorithm/2078-two-furthest-houses-with-different-colors.js
+++ leetcode/algorithm/2078-two-furthest-houses-with-different-colors.js
@@ -25,3 +25,40 @@ var maxDistance = function (colors) {
  * 执行用时 : 3 ms, 在所有 JavaScript 提交中击败了 9.09% 的用户
  * 内存消耗 : 56.12 MB, 在所有 JavaScript 提交中击败了 9.09% 的用户
  */
+/**
+ * 双指针也可以解决吧
+ * @param {*} colors
+ */
+var maxDistance = function (colors) {
+  let left = 0;
+  let right = colors.length - 1;
+  let max = 0;
+  while (left < right) {
+    if (colors[left] !== colors[right]) {
+      max = Math.max(max, right - left);
+    }
+    if (colors[left] === colors[right]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  left = 0;
+  right = colors.length - 1;
+  while (left < right) {
+    if (colors[left] !== colors[right]) {
+      max = Math.max(max, right - left);
+    }
+    if (colors[left] === colors[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return max;
+};
+/**
+ * 双指针也得遍历两次才行
+ * 执行用时 : 2 ms, 在所有 JavaScript 提交中击败了 18.18% 的用户
+ * 内存消耗 : 54.30 MB, 在所有 JavaScript 提交中击败了 18.18% 的用户
+ */
