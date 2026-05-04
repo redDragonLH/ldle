6c011f1faf02f687573740a035fe30acf11c9fb4
liuhe <liuhe@liuhedeMacBook-Pro-2.local>
2026-05-04 09:55:46 +0800

    48. 旋转图像 官方题解

--- leetcode/algorithm/48-rotate-image.js
+++ leetcode/algorithm/48-rotate-image.js
@@ -1,6 +1,6 @@
 /**
  * 48. 旋转图像
- * 
+ *
  * 给定一个 n × n 的二维矩阵表示一个图像。
  * 将图像顺时针旋转 90 度。
  * 说明：
@@ -14,6 +14,23 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function(matrix) {
+var rotate = function (matrix) {};
 
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const n = matrix.length;
+  const matrix_new = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix_new[j][n - i - 1] = matrix[i][j];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix[i][j] = matrix_new[i][j];
+    }
+  }
 };
