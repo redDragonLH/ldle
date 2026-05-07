7ae55440578f9efe2873dcd052329392df9de4ac
liuhe <liuhe@liuhedeMacBook-Pro-2.local>
2026-05-07 10:32:31 +0800

    3660. 跳跃游戏 IX 官方题解

--- leetcode/algorithm/3660-jump-game-ix.js
+++ leetcode/algorithm/3660-jump-game-ix.js
@@ -47,3 +47,46 @@ var maxValue = function (nums) {
 /**
  * 超时,应该可以按照提到条件进行优化，减少不必要的遍历。
  */
+
+/**
+ * 
+ * @param {*} nums 
+ * @returns 
+ */
+var maxValue = function (nums) {
+  const n = nums.length;
+  const ans = new Array(n).fill(0);
+
+  const prevMax = new Array(n);
+
+  nums.reduce(
+    (prev, value, index) => {
+      if (value > prev[0]) {
+        prev = [value, index];
+      }
+      return (prevMax[index] = [...prev]);
+    },
+    [-Infinity, -1],
+  );
+
+  const process = (r, rightMin, rightMax) => {
+    const [pMax, pivotIndex] = prevMax[r];
+    const currMax = pMax <= rightMin ? pMax : rightMax;
+
+    let nextRightMin = Math.min(pMax, rightMin);
+    for (let i = pivotIndex; i <= r; i++) {
+      ans[i] = currMax;
+      nextRightMin = Math.min(nextRightMin, nums[i]);
+    }
+
+    if (pivotIndex === 0) {
+      return;
+    }
+
+    process(pivotIndex - 1, nextRightMin, currMax);
+  };
+
+  process(n - 1, Infinity, 0);
+
+  return ans;
+};
