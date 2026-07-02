99fdbfe44e5bd8bb4f3e58913187352205176a09
liuhe <liuhe@liuhedeMacBook-Pro-4.local>
2026-07-02 09:41:36 +0800

    3286. 穿越网格图的安全路径 深度优先 超时

---
+++ leetcode/algorithm/3286-find-a-safe-walk-through-a-grid.js
@@ -0,0 +1,51 @@
+/**
+ * 3286. 穿越网格图的安全路径
+ *
+ * 给你一个 m x n 的二进制矩形 grid 和一个整数 health 表示你的健康值。
+ * 你开始于矩形的左上角 (0, 0) ，你的目标是矩形的右下角 (m - 1, n - 1) 。
+ * 你可以在矩形中往上下左右相邻格子移动，但前提是你的健康值始终是 正数 。
+ * 对于格子 (i, j) ，如果 grid[i][j] = 1 ，那么这个格子视为 不安全 的，会使你的健康值减少 1 。
+ * 如果你可以到达最终的格子，请你返回 true ，否则返回 false 。
+ * 注意 ，当你在最终格子的时候，你的健康值也必须为 正数 。
+ */
+/**
+ * 深度优先搜索?
+ * @param {number[][]} grid
+ * @param {number} health
+ * @return {boolean}
+ */
+var findSafeWalk = function (grid, health) {
+  const m = grid.length;
+  const n = grid[0].length;
+  const visited = new Set();
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  function dfs(x, y, currentHealth) {
+    if (x < 0 || x >= m || y < 0 || y >= n || currentHealth <= 0) {
+      return false;
+    }
+    const newHealth = currentHealth - grid[x][y];
+    if (x === m - 1 && y === n - 1) {
+      return newHealth > 0;
+    }
+    const key = `${x},${y}`;
+    if (visited.has(key)) {
+      return false;
+    }
+    visited.add(key);
+    for (const [dx, dy] of directions) {
+      if (dfs(x + dx, y + dy, newHealth)) {
+        return true;
+      }
+    }
+    visited.delete(key);
+    return false;
+  }
+
+  return dfs(0, 0, health);
+};
