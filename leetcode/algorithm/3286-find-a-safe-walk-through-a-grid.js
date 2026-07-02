c07a6804e785ce35bca9ef4896617b202f2d9c01
liuhe <liuhe@liuhedeMacBook-Pro-4.local>
2026-07-02 09:47:35 +0800

    3286. 穿越网格图的安全路径 Dijkstra

--- leetcode/algorithm/3286-find-a-safe-walk-through-a-grid.js
+++ leetcode/algorithm/3286-find-a-safe-walk-through-a-grid.js
@@ -49,3 +49,44 @@ var findSafeWalk = function (grid, health) {
 
   return dfs(0, 0, health);
 };
+
+/**
+ * Dijkstra 算法
+ */
+var findSafeWalk = function (grid, health) {
+  const m = grid.length,
+    n = grid[0].length;
+  const dis = Array.from({ length: m }, () => new Array(n).fill(-1));
+  const dirs = [
+    [0, 1],
+    [1, 0],
+    [-1, 0],
+    [0, -1],
+  ];
+
+  const pq = new MinPriorityQueue({
+    compare: (a, b) => a[0] - b[0],
+  });
+  pq.enqueue([grid[0][0], 0, 0]);
+
+  while (!pq.isEmpty()) {
+    const [val, cx, cy] = pq.dequeue();
+    if (dis[cx][cy] >= 0) {
+      continue;
+    }
+    dis[cx][cy] = val;
+
+    for (const [dx, dy] of dirs) {
+      const nx = cx + dx;
+      const ny = cy + dy;
+
+      if (nx < 0 || ny < 0 || nx >= m || ny >= n || dis[nx][ny] >= 0) {
+        continue;
+      }
+
+      pq.enqueue([val + grid[nx][ny], nx, ny]);
+    }
+  }
+
+  return dis[m - 1][n - 1] < health;
+};
