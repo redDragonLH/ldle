deea1f99051964eacbae39f7486e235f3cc08ea4
liuhe <liuhe@liuhedeMacBook-Pro-4.local>
2026-07-08 09:45:27 +0800

    3756. 连接非零数字并乘以其数字和 II 前缀数组

--- leetcode/algorithm/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.js
+++ leetcode/algorithm/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.js
@@ -39,3 +39,38 @@ var sumAndMultiply = function (s, queries) {
   }
   return answer;
 };
+
+const MOD = 1000000007;
+const MAX_N = 100001;
+const pow10 = new Array(MAX_N);
+
+pow10[0] = 1n;
+for (let i = 1; i < MAX_N; ++i) {
+  pow10[i] = (pow10[i - 1] * 10n) % BigInt(MOD);
+}
+
+var sumAndMultiply = function (s, queries) {
+  const n = s.length;
+  const sum = new Array(n + 1).fill(0);
+  const x = new Array(n + 1).fill(0n);
+  const cnt = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; ++i) {
+    const d = s.charCodeAt(i) - 48;
+    sum[i + 1] = sum[i] + d;
+    x[i + 1] = d > 0 ? (x[i] * 10n + BigInt(d)) % BigInt(MOD) : x[i];
+    cnt[i + 1] = cnt[i] + (d > 0 ? 1 : 0);
+  }
+  const m = queries.length;
+  const res = new Array(m);
+  for (let i = 0; i < m; ++i) {
+    const l = queries[i][0];
+    const r = queries[i][1] + 1;
+    const length = cnt[r] - cnt[l];
+    const val_x =
+      (x[r] - ((x[l] * pow10[length]) % BigInt(MOD)) + BigInt(MOD)) %
+      BigInt(MOD);
+    const val_sum = BigInt(sum[r] - sum[l]);
+    res[i] = Number((val_x * val_sum) % BigInt(MOD));
+  }
+  return res;
+};
