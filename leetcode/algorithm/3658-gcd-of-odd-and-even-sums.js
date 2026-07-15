b17999e7ec52cb669f716e5fb4eb989ec4042e48
liuhe <liuhe@liuhedeMacBook-Pro-4.local>
2026-07-15 09:34:37 +0800

    3658. 奇数和与偶数和的最大公约数 数学

---
+++ leetcode/algorithm/3658-gcd-of-odd-and-even-sums.js
@@ -0,0 +1,15 @@
+/**
+ * 3658. 奇数和与偶数和的最大公约数
+ *
+ * 给你一个整数 n。请你计算以下两个值的 最大公约数（GCD）：
+ *  * sumOdd：最小的 n 个正奇数的总和。
+ *  * sumEven：最小的 n 个正偶数的总和。
+ * 返回 sumOdd 和 sumEven 的 GCD。
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function (n) {
+  return n;
+};
