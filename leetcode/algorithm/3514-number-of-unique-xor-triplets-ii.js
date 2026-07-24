43374890f8d93c6993fcf9d7806a3b010121a3ac
liuhe <liuhe@liuhedeMacBook-Pro-4.local>
2026-07-24 09:41:45 +0800

    3514. 不同 XOR 三元组的数目 II 官方题解

--- leetcode/algorithm/3514-number-of-unique-xor-triplets-ii.js
+++ leetcode/algorithm/3514-number-of-unique-xor-triplets-ii.js
@@ -23,3 +23,42 @@ var uniqueXorTriplets = function (nums) {
 /**
  * 超时
  */
+
+var uniqueXorTriplets = function (nums) {
+  let m = 0;
+  for (const v of nums) {
+    m = Math.max(m, v);
+  }
+  let u = 1;
+  while (u <= m) {
+    u <<= 1;
+  }
+  const one = new Array(u).fill(false);
+  const two = new Array(u).fill(false);
+  const three = new Array(u).fill(false);
+  for (const v of nums) {
+    one[v] = true;
+    for (let x = 0; x < u; x++) {
+      if (one[x]) {
+        two[x ^ v] = true;
+      }
+    }
+  }
+  for (const v of nums) {
+    for (let x = 0; x < u; x++) {
+      if (two[x]) {
+        three[x ^ v] = true;
+      }
+    }
+  }
+  let ans = 0;
+  for (const b of three) {
+    if (b) {
+      ans++;
+    }
+  }
+  return ans;
+};
+/**
+ * 官方题解
+ */
